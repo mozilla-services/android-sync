c5592e22880a8bcd91fb3308b63ddf42da8ed579
Richard Newman <rnewman@twinql.com>
2011-11-20 11:42:56 -0800

    License boilerplate and contributor additions.

--- src/main/java/org/mozilla/android/sync/repositories/RepositorySession.java
+++ src/main/java/org/mozilla/android/sync/repositories/RepositorySession.java
@@ -20,6 +20,7 @@
  *
  * Contributor(s):
  * Jason Voll <jvoll@mozilla.com>
+ * Richard Newman <rnewman@mozilla.com>
  *
  * Alternatively, the contents of this file may be used under the terms of
  * either the GNU General Public License Version 2 or later (the "GPL"), or
