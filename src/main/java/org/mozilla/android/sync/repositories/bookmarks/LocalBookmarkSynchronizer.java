1c80eba6876a809a7e696e05564a22269a41840e
jvoll <jvoll@mozilla.com>
2011-11-21 18:44:20 -0800

    Fixed error in LocalBookmarkSynchronizer

--- src/main/java/org/mozilla/android/sync/repositories/bookmarks/LocalBookmarkSynchronizer.java
+++ src/main/java/org/mozilla/android/sync/repositories/bookmarks/LocalBookmarkSynchronizer.java
@@ -86,7 +86,7 @@ public class LocalBookmarkSynchronizer {
     while (curMoz.isAfterLast() == false) {
       
       // Find bookmark in android store
-      int androidId = curMoz.getInt(curMoz.getColumnIndex(BookmarksDatabaseHelper.COL_ANDROID_ID));
+      long androidId = DBUtils.getLongFromCursor(curMoz, BookmarksDatabaseHelper.COL_ANDROID_ID);
       String where = Browser.BookmarkColumns._ID + "=" + androidId;
       Cursor curDroid = context.getContentResolver().query(Browser.BOOKMARKS_URI, null, where, null, null);
       curDroid.moveToFirst();
