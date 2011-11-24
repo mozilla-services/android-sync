4cc0d44621b13eafd90800b560feabc81989dc6c
Richard Newman <rnewman@twinql.com>
2011-11-23 18:01:00 -0800

    Upgrade to sync-crypto 1.2 to get automatic username hashing.

--- external/mvnify.sh
+++ external/mvnify.sh
@@ -1,3 +1,3 @@
 mvn install:install-file -Dfile=httpclientandroidlib-1.0.1.jar -DgroupId=ch.boye.httpclientandroidlib -DartifactId=httpclientandroidlib -Dversion=1.0.1 -Dpackaging=jar
 
-mvn install:install-file -Dfile=sync-crypto-1.1-jar-with-dependencies.jar -DgroupId=org.mozilla.android.sync.crypto -DartifactId=sync-crypto -Dversion=1.1-libs -Dpackaging=jar
+mvn install:install-file -Dfile=sync-crypto-1.2-jar-with-dependencies.jar -DgroupId=org.mozilla.android.sync.crypto -DartifactId=sync-crypto -Dversion=1.2-libs -Dpackaging=jar
