80534b73dbe37b9681cc1eeb8cb114abcd88ffac
Richard Newman <rnewman@twinql.com>
2011-11-29 17:04:49 -0800

    Unused import.

--- src/main/java/org/mozilla/android/sync/repositories/delegates/RepositorySessionFetchRecordsDelegate.java
+++ src/main/java/org/mozilla/android/sync/repositories/delegates/RepositorySessionFetchRecordsDelegate.java
@@ -38,7 +38,6 @@
 
 package org.mozilla.android.sync.repositories.delegates;
 
-import org.mozilla.android.sync.SyncException;
 import org.mozilla.android.sync.repositories.domain.Record;
 
 public interface RepositorySessionFetchRecordsDelegate {
