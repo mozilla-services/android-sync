43f8525914bd9a0b55e5c04112b3bd4421bab6a8
Richard Newman <rnewman@twinql.com>
2011-12-09 18:19:48 -0800

    Adding timestamp handling to RepositorySessionBundle.

--- src/main/java/org/mozilla/android/sync/repositories/RepositorySessionBundle.java
+++ src/main/java/org/mozilla/android/sync/repositories/RepositorySessionBundle.java
@@ -4,4 +4,29 @@ import org.mozilla.android.sync.ExtendedJSONObject;
 
 public class RepositorySessionBundle extends ExtendedJSONObject {
 
+  public RepositorySessionBundle() {
+    super();
+  }
+
+  public RepositorySessionBundle(long lastSyncTimestamp) {
+    this();
+    this.setTimestamp(lastSyncTimestamp);
+  }
+
+  public long getTimestamp() {
+    if (this.containsKey("timestamp")) {
+      return this.getLong("timestamp");
+    }
+    return -1;
+  }
+
+  public void setTimestamp(long timestamp) {
+    this.put("timestamp", new Long(timestamp));
+  }
+
+  public void bumpTimestamp(long timestamp) {
+    if (timestamp > this.getTimestamp()) {
+      this.setTimestamp(timestamp);
+    }
+  }
 }
