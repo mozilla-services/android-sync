49e21fdd8eab073affe9c91826a9678480154e14
Richard Newman <rnewman@twinql.com>
2011-12-10 18:47:33 -0800

    Produce more resources manifests.

--- fennec-merge-jar.sh
+++ fennec-merge-jar.sh
@@ -30,6 +30,8 @@ rsync -av res/layout/*.xml $ANDROID/base/resources/layout/
 rsync -av res/xml/*.xml $ANDROID/base/resources/xml/
 rsync -av strings.xml.in $SYNC/
 find res/drawable* -name '*.png' | sed "s,res/,mobile/android/base/resources/," > $SYNC/android-drawable-resources.mn
+find res/layout    -name '*.xml' | sed "s,res/,mobile/android/base/resources/," > $SYNC/android-layout-resources.mn
+find res/xml       -name '*.xml' | sed "s,res/,mobile/android/base/resources/," > $SYNC/android-xml-resources.mn
 
 echo "Creating README.txt."
 echo $WARNING > $SYNC/README.txt
