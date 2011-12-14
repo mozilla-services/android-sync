40d6b3898df77f7b0a3a230e51399de94a2fd1b0
Yuan Xulei <xyuan@mozilla.com>
2011-12-14 22:07:54 +0800

    Bug 709355 - fix log tags in JpakeRequest.java. r=rnewman

--- src/main/java/org/mozilla/gecko/sync/setup/jpake/JpakeRequest.java
+++ src/main/java/org/mozilla/gecko/sync/setup/jpake/JpakeRequest.java
@@ -11,7 +11,7 @@ import android.util.Log;
 import ch.boye.httpclientandroidlib.HttpEntity;
 
 public class JpakeRequest implements Resource {
-  private static String TAG = "JPAKE_REQUEST";
+  private static String LOG_TAG = "JPAKE_REQUEST";
 
   private BaseResource resource;
   public JpakeRequestDelegate delegate;
@@ -23,7 +23,7 @@ public class JpakeRequest implements Resource {
   public JpakeRequest(URI uri, ResourceDelegate delegate) {
     this.resource = new BaseResource(uri);
     this.resource.delegate = delegate;
-    Log.d(TAG, "new uri: " + uri);
+    Log.d(LOG_TAG, "new uri: " + uri);
   }
 
   @Override
