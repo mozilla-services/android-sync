4baa3b855478344a801a52351b69259b0e9193ae
jvoll <jvoll@mozilla.com>
2011-12-14 00:25:01 -0800

    Cleanup in DefaultFetchDelegate.

--- src/org/mozilla/android/sync/test/helpers/DefaultFetchDelegate.java
+++ src/org/mozilla/android/sync/test/helpers/DefaultFetchDelegate.java
@@ -30,25 +30,6 @@ public class DefaultFetchDelegate extends DefaultDelegate implements RepositoryS
     sharedFail("Hit default delegate");
   }
 
-//  leave in for now, might fine a case where checking guids instead of records makes sense
-//  protected void onDone(ArrayList<Record> records, String[] expected) {
-//    Log.i("rnewman", "onDone. Test Waiter is " + testWaiter());
-//    try {
-//      assertEquals(expected.length, records.size());
-//      for (Record record : records) {
-//        assertFalse(-1 == Arrays.binarySearch(expected, record.guid));
-//      }
-//      Log.i("rnewman", "Notifying success.");
-//      testWaiter().performNotify();
-//    } catch (AssertionError e) {
-//      Log.i("rnewman", "Notifying assertion failure.");
-//      testWaiter().performNotify(e);
-//    } catch (Exception e) {
-//      Log.i("rnewman", "Fucking no.");
-//      testWaiter().performNotify();
-//    }
-//  }
-  
   protected void onDone(ArrayList<Record> records, HashMap<String, Record> expected, long end) {
     Log.i("rnewman", "onDone. Test Waiter is " + testWaiter());
     Log.i("rnewman", "End timestamp is " + end);
