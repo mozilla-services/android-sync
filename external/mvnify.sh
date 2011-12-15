835ee9b3cde6b1c65b122f7dcb10c97db87a1d35
Richard Newman <rnewman@twinql.com>
2011-12-15 10:36:18 -0800

    Bump and rename android-sync-crypto.

--- external/mvnify.sh
+++ external/mvnify.sh
@@ -1,3 +1,3 @@
 mvn install:install-file -Dfile=httpclientandroidlib-1.0.1.jar -DgroupId=ch.boye.httpclientandroidlib -DartifactId=httpclientandroidlib -Dversion=1.0.1 -Dpackaging=jar
 
-mvn install:install-file -Dfile=sync-crypto-1.3-jar-with-dependencies.jar -DgroupId=org.mozilla.gecko.sync.crypto -DartifactId=sync-crypto -Dversion=1.3-libs -Dpackaging=jar
+mvn install:install-file -Dfile=android-sync-crypto-1.4-jar-with-dependencies.jar -DgroupId=org.mozilla.android.sync -DartifactId=android-sync-crypto -Dversion=1.4-libs -Dpackaging=jar
