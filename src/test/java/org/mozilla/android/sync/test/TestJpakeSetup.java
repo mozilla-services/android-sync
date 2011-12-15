3de81d57d8804d9921f57aa09550d2f6cc38bf86
Chenxia Liu <liuche@mozilla.com>
2011-12-15 11:08:27 -0500

    Added JPake test

---
+++ src/test/java/org/mozilla/android/sync/test/TestJpakeSetup.java
@@ -0,0 +1,15 @@
+package org.mozilla.android.sync.test;
+
+import org.junit.Test;
+
+public class TestJpakeSetup {
+  
+  @Test
+  public void testKeyDerivation() {
+    String keyChars = "0123456789abcdef0123456789abcdef";
+    String expectedCryptoBytes = "5300843cc20d568881471597524f125c8fe3f806a548cefd053226ddb5f40fe8";
+    String expectedHmac = "1f1f0b85c3418c2b26a68a0a4049e292585a0568bd1c8add97e66dbb65b8be99";
+    
+    
+  }
+}
