3b1b9f6b7feaf079ad37fee8f3d179a7e6738d33
Richard Newman <rnewman@twinql.com>
2011-12-15 11:49:25 -0800

    Add TestGlobalSession.

---
+++ src/org/mozilla/android/sync/stage/test/TestGlobalSession.java
@@ -0,0 +1,63 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+package org.mozilla.android.sync.stage.test;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
+import static junit.framework.Assert.fail;
+
+import android.test.AndroidTestCase;
+
+import org.mozilla.android.sync.crypto.KeyBundle;
+import org.mozilla.gecko.sync.GlobalSession;
+import org.mozilla.gecko.sync.delegates.GlobalSessionCallback;
+import org.mozilla.gecko.sync.stage.GlobalSyncStage.Stage;
+
+public class TestGlobalSession extends AndroidTestCase {
+
+  public void testStageAdvance() {
+    assertEquals(GlobalSession.nextStage(Stage.idle), Stage.checkPreconditions);
+    assertEquals(GlobalSession.nextStage(Stage.completed), Stage.idle);
+  }
+
+  protected class HappyCallback implements GlobalSessionCallback {
+    int stageCounter = Stage.values().length - 1;    // Exclude starting state.
+    public boolean calledSuccess = false;
+
+    public void handleError(GlobalSession globalSession, Exception ex) {
+      ex.printStackTrace();
+      fail("No error should occur.");
+    }
+
+    public void handleSuccess(GlobalSession globalSession) {
+      assertEquals(0, stageCounter);
+      calledSuccess = true;
+    }
+
+    public void handleStageCompleted(Stage currentState,
+                                     GlobalSession globalSession) {
+      stageCounter--;
+    }
+  }
+
+  public void testCallbacks() {
+    String clusterURL = "http://localhost:8080/";
+    String username   = "johndoe";
+    String password   = "password";
+    String syncKey    = "abcdeabcdeabcdeabcdeabcdea";
+    KeyBundle syncKeyBundle = new KeyBundle(username, syncKey);
+    HappyCallback callback = new HappyCallback();
+    GlobalSession session;
+    try {
+      // TODO: context?
+      session = new MockGlobalSession(clusterURL, username, password, syncKeyBundle, callback, null);
+      session.start();
+      assertTrue(callback.calledSuccess);
+    } catch (Exception e) {
+      e.printStackTrace();
+      fail("Got exception.");
+    }
+
+  }
+}
