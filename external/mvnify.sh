e5bcab8f9731a9b77d7695eda7f0d69d6ecff1b8
Richard Newman <rnewman@twinql.com>
2011-12-18 16:39:02 -0800

    Bump android-sync-crypto dependency to 1.6 to allow more appropriate use of Base64.

--- external/mvnify.sh
+++ external/mvnify.sh
@@ -1,3 +1,3 @@
 mvn install:install-file -Dfile=httpclientandroidlib-1.0.1.jar -DgroupId=ch.boye.httpclientandroidlib -DartifactId=httpclientandroidlib -Dversion=1.0.1 -Dpackaging=jar
 
-mvn install:install-file -Dfile=android-sync-crypto-1.5-jar-with-dependencies.jar -DgroupId=org.mozilla.android.sync -DartifactId=android-sync-crypto -Dversion=1.5-libs -Dpackaging=jar
+mvn install:install-file -Dfile=android-sync-crypto-1.6-jar-with-dependencies.jar -DgroupId=org.mozilla.android.sync -DartifactId=android-sync-crypto -Dversion=1.6-libs -Dpackaging=jar
