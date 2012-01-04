44d9636f14f53cb112f5669de8310b462dd3a2ba
Chenxia Liu <liuche@mozilla.com>
2012-01-03 19:23:31 -0800

    Adding pairWithPin feature, for connecting new devices to Sync account.

--- src/main/java/org/mozilla/gecko/sync/setup/activities/AccountActivity.java
+++ src/main/java/org/mozilla/gecko/sync/setup/activities/AccountActivity.java
@@ -50,6 +50,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
@@ -189,5 +190,10 @@ public class AccountActivity extends AccountAuthenticatorActivity {
     Intent intent = new Intent(mContext, SetupSuccessActivity.class);
     startActivity(intent);
     finish();
+  }
+
+  private void activateButton(Button button, boolean toActivate) {
+    button.setEnabled(toActivate);
+    button.setClickable(toActivate);
   }
 }
