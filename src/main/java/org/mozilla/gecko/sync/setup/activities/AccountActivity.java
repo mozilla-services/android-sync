bdabfb2330dc6b1e6a01075c556ceae1e69d7b68
Chenxia Liu <liuche@mozilla.com>
2012-01-06 12:30:43 -0800

    Bug 711868 - validate account credentials to avoid account creation crashing.

--- src/main/java/org/mozilla/gecko/sync/setup/activities/AccountActivity.java
+++ src/main/java/org/mozilla/gecko/sync/setup/activities/AccountActivity.java
@@ -48,6 +48,8 @@ import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -55,16 +57,26 @@ import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.EditText;
-import android.widget.TextView;
 
 public class AccountActivity extends AccountAuthenticatorActivity {
-  private final static String LOG_TAG = "AccountActivity";
-  private AccountManager mAccountManager;
-  private Context mContext;
-  private String username;
-  private String password;
-  private String key;
-  private String server;
+  private final static String LOG_TAG        = "AccountActivity";
+
+  private final static String DEFAULT_SERVER = "https://setup.services.mozilla.com/";
+
+  private AccountManager      mAccountManager;
+  private Context             mContext;
+  private String              username;
+  private String              password;
+  private String              key;
+  private String              server;
+
+  // UI elements.
+  private EditText            serverInput;
+  private EditText            usernameInput;
+  private EditText            passwordInput;
+  private EditText            synckeyInput;
+  private CheckBox            serverCheckbox;
+  private Button              connectButton;
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
@@ -72,34 +84,45 @@ public class AccountActivity extends AccountAuthenticatorActivity {
     setContentView(R.layout.sync_account);
     mContext = getApplicationContext();
     mAccountManager = AccountManager.get(getApplicationContext());
-  }
 
-  protected void toggleServerField(boolean enabled) {
-    TextView serverField = (TextView) findViewById(R.id.server);
-    Log.i(LOG_TAG, "Toggling checkbox: " + enabled);
-    serverField.setFocusable(enabled);
-    serverField.setClickable(enabled);
+    // Find UI elements.
+    usernameInput = (EditText) findViewById(R.id.usernameInput);
+    passwordInput = (EditText) findViewById(R.id.passwordInput);
+    synckeyInput = (EditText) findViewById(R.id.keyInput);
+    serverInput = (EditText) findViewById(R.id.serverInput);
+
+    TextWatcher inputValidator = makeInputValidator();
+
+    usernameInput.addTextChangedListener(inputValidator);
+    passwordInput.addTextChangedListener(inputValidator);
+    synckeyInput.addTextChangedListener(inputValidator);
+    serverInput.addTextChangedListener(inputValidator);
+
+    connectButton = (Button) findViewById(R.id.accountConnectButton);
+    serverCheckbox = (CheckBox) findViewById(R.id.checkbox_server);
+
+    serverCheckbox.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+      @Override
+      public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        Log.i(LOG_TAG, "Toggling checkbox: " + isChecked);
+        activateView(serverInput, isChecked);
+        if (!isChecked) { // Clear server input.
+          serverInput.setText("");
+        }
+        // Activate connectButton if necessary.
+        activateView(connectButton, validateInputs());
+      }
+    });
   }
 
   @Override
   public void onStart() {
     super.onStart();
     // Start with an empty form
-    setContentView(R.layout.sync_account);
-    CheckBox serverCheckbox = (CheckBox) findViewById(R.id.checkbox_server);
-
-    Log.i(LOG_TAG, "Setting onCheckedChangeListener.");
-    OnCheckedChangeListener listener = new OnCheckedChangeListener() {
-
-      @Override
-      public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-        Log.i(LOG_TAG, "Toggling checkbox: " + isChecked);
-        toggleServerField(isChecked);
-      }
-    };
-    serverCheckbox.setOnCheckedChangeListener(listener);
-    // Enable or disable accordingly.
-    listener.onCheckedChanged(serverCheckbox, serverCheckbox.isChecked());
+    usernameInput.setText("");
+    passwordInput.setText("");
+    synckeyInput.setText("");
+    passwordInput.setText("");
   }
 
   public void cancelClickHandler(View target) {
@@ -112,19 +135,58 @@ public class AccountActivity extends AccountAuthenticatorActivity {
    */
   public void connectClickHandler(View target) {
     Log.d(LOG_TAG, "connectClickHandler for view " + target);
-    username = ((EditText) findViewById(R.id.username)).getText().toString();
-    password = ((EditText) findViewById(R.id.password)).getText().toString();
-    key = ((EditText) findViewById(R.id.key)).getText().toString();
-    CheckBox serverCheckbox = (CheckBox) findViewById(R.id.checkbox_server);
-    EditText serverField = (EditText) findViewById(R.id.server);
+    username = usernameInput.getText().toString();
+    password = passwordInput.getText().toString();
+    key = synckeyInput.getText().toString();
     if (serverCheckbox.isChecked()) {
-      server = serverField.getText().toString();
+      server = serverInput.getText().toString();
     }
+    enableCredEntry(false);
 
     // TODO : Authenticate with Sync Service, once implemented, with
     // onAuthSuccess as callback
 
     authCallback();
+  }
+
+  /* Helper UI functions */
+  private void enableCredEntry(boolean toEnable) {
+    usernameInput.setEnabled(toEnable);
+    passwordInput.setEnabled(toEnable);
+    synckeyInput.setEnabled(toEnable);
+    if (!toEnable) {
+      serverInput.setEnabled(toEnable);
+    } else {
+      serverInput.setEnabled(serverCheckbox.isChecked());
+    }
+  }
+
+  private TextWatcher makeInputValidator() {
+    return new TextWatcher() {
+
+      @Override
+      public void afterTextChanged(Editable s) {
+        activateView(connectButton, validateInputs());
+      }
+
+      @Override
+      public void beforeTextChanged(CharSequence s, int start, int count,
+          int after) {
+      }
+
+      @Override
+      public void onTextChanged(CharSequence s, int start, int before, int count) {
+      }
+    };
+  }
+
+  private boolean validateInputs() {
+    if (usernameInput.length() == 0 || passwordInput.length() == 0
+        || synckeyInput.length() == 0
+        || (serverCheckbox.isChecked() && serverInput.length() == 0)) {
+      return false;
+    }
+    return true;
   }
 
   /*
@@ -133,13 +195,15 @@ public class AccountActivity extends AccountAuthenticatorActivity {
   private void authCallback() {
     // Create and add account to AccountManager
     // TODO: only allow one account to be added?
-    final Intent intent = createAccount(mAccountManager, username, key, password, server);
+    final Intent intent = createAccount(mAccountManager, username, key,
+        password, server);
     setAccountAuthenticatorResult(intent.getExtras());
 
     // Testing out the authFailure case
-    //authFailure();
+    // authFailure();
 
-    // TODO: Currently, we do not actually authenticate username/pass against Moz sync server.
+    // TODO: Currently, we do not actually authenticate username/pass against
+    // Moz sync server.
 
     // Successful authentication result
     setResult(RESULT_OK, intent);
@@ -153,7 +217,8 @@ public class AccountActivity extends AccountAuthenticatorActivity {
   }
 
   // TODO: lift this out.
-  public static Intent createAccount(AccountManager accountManager, String username, String syncKey, String password, String serverURL) {
+  public static Intent createAccount(AccountManager accountManager,
+      String username, String syncKey, String password, String serverURL) {
     final Account account = new Account(username, Constants.ACCOUNTTYPE_SYNC);
     final Bundle userbundle = new Bundle();
 
@@ -162,6 +227,8 @@ public class AccountActivity extends AccountAuthenticatorActivity {
     if (serverURL != null) {
       Log.i(LOG_TAG, "Setting explicit server URL: " + serverURL);
       userbundle.putString(Constants.OPTION_SERVER, serverURL);
+    } else {
+      userbundle.putString(Constants.OPTION_SERVER, DEFAULT_SERVER);
     }
     accountManager.addAccountExplicitly(account, password, userbundle);
 
@@ -169,19 +236,22 @@ public class AccountActivity extends AccountAuthenticatorActivity {
 
     // Set components to sync (default: all).
     ContentResolver.setMasterSyncAutomatically(true);
-    ContentResolver.setSyncAutomatically(account, Authorities.BROWSER_AUTHORITY, true);
+    ContentResolver.setSyncAutomatically(account,
+        Authorities.BROWSER_AUTHORITY, true);
     // TODO: add other ContentProviders as needed (e.g. passwords)
     // TODO: for each, also add to res/xml to make visible in account settings
     Log.d(LOG_TAG, "Finished setting syncables.");
 
     final Intent intent = new Intent();
     intent.putExtra(AccountManager.KEY_ACCOUNT_NAME, username);
-    intent.putExtra(AccountManager.KEY_ACCOUNT_TYPE, Constants.ACCOUNTTYPE_SYNC);
+    intent
+        .putExtra(AccountManager.KEY_ACCOUNT_TYPE, Constants.ACCOUNTTYPE_SYNC);
     intent.putExtra(AccountManager.KEY_AUTHTOKEN, Constants.ACCOUNTTYPE_SYNC);
     return intent;
   }
 
   private void authFailure() {
+    enableCredEntry(true);
     Intent intent = new Intent(mContext, SetupFailureActivity.class);
     startActivity(intent);
   }
@@ -192,8 +262,8 @@ public class AccountActivity extends AccountAuthenticatorActivity {
     finish();
   }
 
-  private void activateButton(Button button, boolean toActivate) {
-    button.setEnabled(toActivate);
-    button.setClickable(toActivate);
+  private void activateView(View view, boolean toActivate) {
+    view.setEnabled(toActivate);
+    view.setClickable(toActivate);
   }
 }
