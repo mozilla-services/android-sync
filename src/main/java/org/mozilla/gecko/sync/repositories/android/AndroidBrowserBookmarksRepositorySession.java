3c6af97ad6cb0d8b0503659020244c34f4eef118
Richard Newman <rnewman@twinql.com>
2012-01-13 23:58:05 -0800

    Improve parenting code.

--- src/main/java/org/mozilla/gecko/sync/repositories/android/AndroidBrowserBookmarksRepositorySession.java
+++ src/main/java/org/mozilla/gecko/sync/repositories/android/AndroidBrowserBookmarksRepositorySession.java
@@ -113,6 +113,9 @@ public class AndroidBrowserBookmarksRepositorySession extends AndroidBrowserRepo
   }
 
   private String getParentName(String parentGUID) throws ParentNotFoundException, NullCursorException {
+    if (parentGUID == null) {
+      return "";
+    }
     if (RepoUtils.SPECIAL_GUIDS_MAP.containsKey(parentGUID)) {
       return RepoUtils.SPECIAL_GUIDS_MAP.get(parentGUID);
     }
@@ -151,6 +154,7 @@ public class AndroidBrowserBookmarksRepositorySession extends AndroidBrowserRepo
       trace("Expecting " + count + " children.");
 
       // Track badly positioned records.
+      // TODO: use a mechanism here that preserves ordering.
       HashMap<String, Long> broken = new HashMap<String, Long>();
 
       // Get children into array in correct order.
@@ -219,12 +223,15 @@ public class AndroidBrowserBookmarksRepositorySession extends AndroidBrowserRepo
         Log.e(LOG_TAG, "Have the parent android ID for the record but the parent's GUID wasn't found.");
         throw new NoGuidForIdException(null);
       }
-      return RepoUtils.bookmarkFromMirrorCursor(cur, null, "", null);
     }
 
-    String parentName = getParentName(androidParentGUID);
-
     // If record is a folder, build out the children array.
+    JSONArray childArray = getChildArrayForCursor(cur, recordGUID);
+    String parentName = getParentName(androidParentGUID);
+    return RepoUtils.bookmarkFromMirrorCursor(cur, androidParentGUID, parentName, childArray);
+  }
+
+  protected JSONArray getChildArrayForCursor(Cursor cur, String recordGUID) throws NullCursorException {
     JSONArray childArray = null;
     boolean isFolder = rowIsFolder(cur);
     Log.d(LOG_TAG, "Record " + recordGUID + " is a " + (isFolder ? "folder." : "bookmark."));
@@ -235,7 +242,7 @@ public class AndroidBrowserBookmarksRepositorySession extends AndroidBrowserRepo
     if (childArray != null) {
       Log.d(LOG_TAG, "Fetched " + childArray.size() + " children for " + recordGUID);
     }
-    return RepoUtils.bookmarkFromMirrorCursor(cur, androidParentGUID, parentName, childArray);
+    return childArray;
   }
 
   @Override
