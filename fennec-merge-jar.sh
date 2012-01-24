47956ef62fa7869b67dcb8107f333c992d8e4fd1
Marina Samuel <marina.samuel25@gmail.com>
2012-01-23 16:37:13 -0800

    Bug 709660 - Split drawable manifest into 4

--- fennec-merge-jar.sh
+++ fennec-merge-jar.sh
@@ -33,9 +33,12 @@ rsync -av strings.xml.in $SYNC/
 rsync -av sync_strings.dtd.in $ANDROID/base/locales/en-US/sync_strings.dtd
 
 echo "res/values/sync_styles.xml " > $SYNC/android-values-resources.mn
-find res/layout    -name '*.xml' > $SYNC/android-layout-resources.mn
-find res/drawable* -name '*.png' | sed "s,res/,mobile/android/base/resources/," > $SYNC/android-drawable-resources.mn
-find res/xml       -name '*.xml' | sed "s,res/,mobile/android/base/resources/," > $SYNC/android-xml-resources.mn
+find res/layout         -name '*.xml' > $SYNC/android-layout-resources.mn
+find res/drawable       -name '*.png' | sed "s,res/,mobile/android/base/resources/," > $SYNC/android-drawable-resources.mn
+find res/drawable-ldpi  -name '*.png' | sed "s,res/,mobile/android/base/resources/," > $SYNC/android-drawable-ldpi-resources.mn
+find res/drawable-mdpi  -name '*.png' | sed "s,res/,mobile/android/base/resources/," > $SYNC/android-drawable-mdpi-resources.mn
+find res/drawable-hdpi  -name '*.png' | sed "s,res/,mobile/android/base/resources/," > $SYNC/android-drawable-hdpi-resources.mn
+find res/xml            -name '*.xml' | sed "s,res/,mobile/android/base/resources/," > $SYNC/android-xml-resources.mn
 
 echo "Creating README.txt."
 echo $WARNING > $SYNC/README.txt
