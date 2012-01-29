91c5e2344877dc89995372a3eb09d0a6cdb8cf4f
Richard Newman <rnewman@twinql.com>
2012-01-28 16:29:52 -0800

    Bug 719693 - Effect review comments on 6ffb7d72f.

--- src/main/java/org/mozilla/gecko/sync/net/SyncResponse.java
+++ src/main/java/org/mozilla/gecko/sync/net/SyncResponse.java
@@ -20,6 +20,7 @@
  *
  * Contributor(s):
  *   Richard Newman <rnewman@mozilla.com>
+ *   Nick Alexander <nalexander@mozilla.com>
  *
  * Alternatively, the contents of this file may be used under the terms of
  * either the GNU General Public License Version 2 or later (the "GPL"), or
@@ -40,7 +41,6 @@ package org.mozilla.gecko.sync.net;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.Date;
 import java.util.Scanner;
 
 import org.json.simple.parser.ParseException;
@@ -48,6 +48,7 @@ import org.mozilla.gecko.sync.ExtendedJSONObject;
 import org.mozilla.gecko.sync.NonObjectJSONException;
 import org.mozilla.gecko.sync.Utils;
 
+import android.util.Log;
 import ch.boye.httpclientandroidlib.Header;
 import ch.boye.httpclientandroidlib.HttpEntity;
 import ch.boye.httpclientandroidlib.HttpResponse;
@@ -55,6 +56,8 @@ import ch.boye.httpclientandroidlib.impl.cookie.DateParseException;
 import ch.boye.httpclientandroidlib.impl.cookie.DateUtils;
 
 public class SyncResponse {
+  private static final String HEADER_RETRY_AFTER = "retry-after";
+  private static final String LOG_TAG = "SyncResponse";
 
   protected HttpResponse response;
 
@@ -126,10 +129,20 @@ public class SyncResponse {
     return this.response.containsHeader(h);
   }
 
-  private int getIntegerHeader(String h) {
+  private static boolean missingHeader(String value) {
+    return value == null ||
+           value.isEmpty();
+  }
+
+  private int getIntegerHeader(String h) throws NumberFormatException {
     if (this.hasHeader(h)) {
       Header header = this.response.getFirstHeader(h);
-      return Integer.parseInt(header.getValue(), 10);
+      String value  = header.getValue();
+      if (missingHeader(value)) {
+        Log.w(LOG_TAG, h + " header present but empty.");
+        return -1;
+      }
+      return Integer.parseInt(value, 10);
     }
     return -1;
   }
@@ -138,25 +151,31 @@ public class SyncResponse {
    * @return A number of seconds, or -1 if the header was not present.
    */
   public int retryAfter() throws NumberFormatException {
-    if (!this.hasHeader("retry-after")) {
+    if (!this.hasHeader(HEADER_RETRY_AFTER)) {
       return -1;
     }
 
-    Header header = this.response.getFirstHeader("retry-after");
+    Header header = this.response.getFirstHeader(HEADER_RETRY_AFTER);
+    String retryAfter = header.getValue();
+    if (missingHeader(retryAfter)) {
+      Log.w(LOG_TAG, "Retry-After header present but empty.");
+      return -1;
+    }
 
     try {
-      return Integer.parseInt(header.getValue(), 10);
+      return Integer.parseInt(retryAfter, 10);
     } catch (NumberFormatException e) {
+      // Fall through to try date format.
     }
 
     try {
-      final Date date = DateUtils.parseDate(header.getValue());
-      final Date now  = new Date();
-      return (int)((date.getTime() - now.getTime()) / 1000); // Convert milliseconds to seconds.
+      final long then = DateUtils.parseDate(retryAfter).getTime();
+      final long now  = System.currentTimeMillis();
+      return (int)((then - now) / 1000);     // Convert milliseconds to seconds.
     } catch (DateParseException e) {
+      Log.w(LOG_TAG, "Retry-After header neither integer nor date: " + retryAfter);
+      return -1;
     }
-
-    return -1;
   }
 
   public int weaveBackoff() throws NumberFormatException {
@@ -195,5 +214,4 @@ public class SyncResponse {
     }
     return null;
   }
-
 }
