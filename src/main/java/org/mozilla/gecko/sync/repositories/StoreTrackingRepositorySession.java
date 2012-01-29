33241f566b17a00c98609698093e04a2beb8d86e
Richard Newman <rnewman@twinql.com>
2012-01-29 14:22:03 -0800

    Bug 722945 - Introduce StoreTracker, StoreTrackingRepositorySession.

---
+++ src/main/java/org/mozilla/gecko/sync/repositories/StoreTrackingRepositorySession.java
@@ -0,0 +1,46 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.gecko.sync.repositories;
+
+import org.mozilla.gecko.sync.repositories.delegates.RepositorySessionBeginDelegate;
+import org.mozilla.gecko.sync.repositories.delegates.RepositorySessionFinishDelegate;
+
+public abstract class StoreTrackingRepositorySession extends RepositorySession {
+  protected StoreTracker storeTracker;
+
+  protected static StoreTracker createStoreTracker() {
+    return new HashSetStoreTracker();
+  }
+
+  public StoreTrackingRepositorySession(Repository repository) {
+    super(repository);
+  }
+
+  @Override
+  public void begin(RepositorySessionBeginDelegate delegate) {
+    RepositorySessionBeginDelegate deferredDelegate = delegate.deferredBeginDelegate(delegateQueue);
+    try {
+      super.sharedBegin();
+    } catch (InvalidSessionTransitionException e) {
+      deferredDelegate.onBeginFailed(e);
+      return;
+    }
+    // Or do this in your own subclass.
+    storeTracker = createStoreTracker();
+    deferredDelegate.onBeginSucceeded(this);
+  }
+
+  @Override
+  public void abort(RepositorySessionFinishDelegate delegate) {
+    this.storeTracker = null;
+    super.abort(delegate);
+  }
+
+  @Override
+  public void finish(RepositorySessionFinishDelegate delegate) {
+    this.storeTracker = null;
+    super.finish(delegate);
+  }
+}
