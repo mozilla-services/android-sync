e830dbad0520f76f78f040c40cae159923b42cd1
Richard Newman <rnewman@twinql.com>
2012-02-05 23:51:52 -0800

    Lift simple success delegates for reuse. Oh, dear. Test code is breeding.

---
+++ test/src/org/mozilla/android/sync/test/helpers/simple/SimpleSuccessBeginDelegate.java
@@ -0,0 +1,24 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */package org.mozilla.android.sync.test.helpers.simple;
+
+import java.util.concurrent.ExecutorService;
+
+import junit.framework.AssertionFailedError;
+
+import org.mozilla.android.sync.test.AndroidBrowserRepositoryTestHelper;
+import org.mozilla.gecko.sync.repositories.delegates.RepositorySessionBeginDelegate;
+
+public abstract class SimpleSuccessBeginDelegate implements RepositorySessionBeginDelegate {
+  @Override
+  public void onBeginFailed(Exception ex) {
+    final AssertionFailedError e = new AssertionFailedError("Begin failed: " + ex.getMessage());
+    e.initCause(ex);
+    AndroidBrowserRepositoryTestHelper.testWaiter.performNotify(e);
+  }
+
+  @Override
+  public RepositorySessionBeginDelegate deferredBeginDelegate(ExecutorService executor) {
+    return this;
+  }
+}
---
+++ test/src/org/mozilla/android/sync/test/helpers/simple/SimpleSuccessCreationDelegate.java
@@ -0,0 +1,27 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.android.sync.test.helpers.simple;
+
+import junit.framework.AssertionFailedError;
+
+import org.mozilla.android.sync.test.AndroidBrowserRepositoryTestHelper;
+import org.mozilla.gecko.sync.repositories.delegates.RepositorySessionCreationDelegate;
+
+import android.util.Log;
+
+public abstract class SimpleSuccessCreationDelegate implements RepositorySessionCreationDelegate {
+  @Override
+  public void onSessionCreateFailed(Exception ex) {
+    Log.w("SimpleSuccessCreationDelegate", "Session creation failed.", ex);
+    final AssertionFailedError e = new AssertionFailedError("Session creation failed: " + ex.getMessage());
+    e.initCause(ex);
+    AndroidBrowserRepositoryTestHelper.testWaiter.performNotify(e);
+  }
+
+  @Override
+  public RepositorySessionCreationDelegate deferredCreationDelegate() {
+    return this;
+  }
+}
