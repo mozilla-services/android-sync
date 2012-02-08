8ef4c50489235d45f4ab9e72ad3830b1c74046da
Nick Alexander <nalexander@mozilla.com>
2012-02-08 12:00:59 -0800

    Remove SyncCryptographer - this functionality is tested in TestCollectionKeys.testCryptoRecordFromCollectionKeys().

--- src/test/java/org/mozilla/android/sync/test/TestSyncCryptographer.java
+++ src/test/java/org/mozilla/android/sync/test/TestSyncCryptographer.java
@@ -102,34 +102,4 @@ public class TestSyncCryptographer {
         // Check the status to make sure it worked
         assertEquals(CryptoStatus.OK, result.getStatus());
     }
-
-    @Test
-    public void testCreateKeysBundle() throws CryptoException, UnsupportedEncodingException {
-        String username =                       "b6evr62dptbxz7fvebek7btljyu322wp";
-        String friendlyBase32SyncKey =          "basuxv2426eqj7frhvpcwkavdi";
-
-        // Generate keys
-        SyncCryptographer cryptographer = new SyncCryptographer(username, friendlyBase32SyncKey);
-        CryptoStatusBundle result = cryptographer.generateCryptoKeysWBOPayload();
-
-        // Check result status and get generated keys
-        assertEquals(CryptoStatus.OK, result.getStatus());
-        KeyBundle createdKeys = cryptographer.getKeys();
-
-        // Decrypt what we created to see the keys as they were stored in the json
-        ExtendedJSONObject in = new ExtendedJSONObject();
-        in.put("payload", result.getJson());
-        in.put("id", "keys");
-        result = cryptographer.decryptWBO(in.toJSONString());
-
-        // Check result and get decrypted keys
-        KeyBundle decryptedKeys = cryptographer.getKeys();
-        assertEquals(CryptoStatus.OK, result.getStatus());
-
-        // Compare decrypted keys to the keys that were set upon creation
-        boolean equal = Arrays.equals(createdKeys.getEncryptionKey(), decryptedKeys.getEncryptionKey());
-        assertEquals(true, equal);
-        equal = Arrays.equals(createdKeys.getHMACKey(), decryptedKeys.getHMACKey());
-        assertEquals(true, equal);
-    }
 }
