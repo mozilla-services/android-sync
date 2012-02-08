1928aaea49806747911d134e690638de98020d90
Nick Alexander <nalexander@mozilla.com>
2012-02-08 15:25:40 -0800

    Remove Cryptographer - consolidate tests in TestKeyBundle.

--- src/test/java/org/mozilla/gecko/sync/crypto/test/TestKeyBundle.java
+++ src/test/java/org/mozilla/gecko/sync/crypto/test/TestKeyBundle.java
@@ -1,40 +1,38 @@
-/* Any copyright is dedicated to the Public Domain.
-   http://creativecommons.org/publicdomain/zero/1.0/ */
-
 package org.mozilla.gecko.sync.crypto.test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.io.UnsupportedEncodingException;
+import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 
 import org.junit.Test;
 import org.mozilla.apache.commons.codec.binary.Base64;
+import org.mozilla.gecko.sync.Utils;
 import org.mozilla.gecko.sync.crypto.CryptoException;
 import org.mozilla.gecko.sync.crypto.KeyBundle;
 
-public class TestCryptographer {
+public class TestKeyBundle {
+  @Test
+  public void testUsernameFromAccount() throws NoSuchAlgorithmException, UnsupportedEncodingException {
+    assertEquals(Utils.sha1Base32("foobar@baz.com"), "xee7ffonluzpdp66l6xgpyh2v2w6ojkc");
+    assertEquals(KeyBundle.usernameFromAccount("foobar@baz.com"), "xee7ffonluzpdp66l6xgpyh2v2w6ojkc");
+    assertEquals(KeyBundle.usernameFromAccount("foobar"), "foobar");
+    assertEquals(KeyBundle.usernameFromAccount("xee7ffonluzpdp66l6xgpyh2v2w6ojkc"), "xee7ffonluzpdp66l6xgpyh2v2w6ojkc");
+  }
 
   @Test
   public void testCreateKeyBundle() throws UnsupportedEncodingException, CryptoException {
-    String username =
-        "smqvooxj664hmrkrv6bw4r4vkegjhkns";
-    String friendlyBase32SyncKey =
-        "gbh7teqqcgyzd65svjgibd7tqy";
-    String base64EncryptionKey =
-        "069EnS3EtDK4y1tZ1AyKX+U7WEsWRp9b" +
-        "RIKLdW/7aoE=";
-    String base64HmacKey =
-        "LF2YCS1QCgSNCf0BCQvQ06SGH8jqJDi9" +
-        "dKj0O+b0fwI=";
+    String username              = "smqvooxj664hmrkrv6bw4r4vkegjhkns";
+    String friendlyBase32SyncKey = "gbh7teqqcgyzd65svjgibd7tqy";
+    String base64EncryptionKey   = "069EnS3EtDK4y1tZ1AyKX+U7WEsWRp9b" +
+                                   "RIKLdW/7aoE=";
+    String base64HmacKey         = "LF2YCS1QCgSNCf0BCQvQ06SGH8jqJDi9" +
+                                   "dKj0O+b0fwI=";
 
     KeyBundle keys = new KeyBundle(username, friendlyBase32SyncKey);
-
-    // Check Encryption Key
     assertTrue(Arrays.equals(keys.getEncryptionKey(), Base64.decodeBase64(base64EncryptionKey.getBytes("UTF-8"))));
-
-    // Check HMAC Key
     assertTrue(Arrays.equals(keys.getHMACKey(), Base64.decodeBase64(base64HmacKey.getBytes("UTF-8"))));
   }
 
