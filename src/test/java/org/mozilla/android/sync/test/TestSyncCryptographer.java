92481f360e479e56f20991063adee8364e58d2d6
Nick Alexander <nalexander@mozilla.com>
2012-02-11 15:26:28 -0800

    Remove SyncCryptographer - pull the trigger.

--- src/test/java/org/mozilla/android/sync/test/TestSyncCryptographer.java
+++
@@ -1,7 +0,0 @@
-/* Any copyright is dedicated to the Public Domain.
-   http://creativecommons.org/publicdomain/zero/1.0/ */
-
-package org.mozilla.android.sync.test;
-
-public class TestSyncCryptographer {
-}
