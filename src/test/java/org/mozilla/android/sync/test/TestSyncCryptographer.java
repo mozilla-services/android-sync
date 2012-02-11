1fe3427af5584b5f8c3c3bf7e105f51bd52c12ac
Nick Alexander <nalexander@mozilla.com>
2012-02-11 15:26:28 -0800

    Remove SyncCryptographer - move testCreateKeysBundle from TestSyncCryptographer to TestCollectionKeys.

--- src/test/java/org/mozilla/android/sync/test/TestSyncCryptographer.java
+++ src/test/java/org/mozilla/android/sync/test/TestSyncCryptographer.java
@@ -3,48 +3,5 @@
 
 package org.mozilla.android.sync.test;
 
-import static org.junit.Assert.assertEquals;
-
-import java.io.UnsupportedEncodingException;
-import java.util.Arrays;
-
-import org.junit.Test;
-import org.mozilla.gecko.sync.ExtendedJSONObject;
-import org.mozilla.gecko.sync.crypto.CryptoException;
-import org.mozilla.gecko.sync.crypto.KeyBundle;
-import org.mozilla.gecko.sync.cryptographer.CryptoStatusBundle;
-import org.mozilla.gecko.sync.cryptographer.CryptoStatusBundle.CryptoStatus;
-import org.mozilla.gecko.sync.cryptographer.SyncCryptographer;
-
 public class TestSyncCryptographer {
-
-    @Test
-    public void testCreateKeysBundle() throws CryptoException, UnsupportedEncodingException {
-        String username =                       "b6evr62dptbxz7fvebek7btljyu322wp";
-        String friendlyBase32SyncKey =          "basuxv2426eqj7frhvpcwkavdi";
-
-        // Generate keys
-        SyncCryptographer cryptographer = new SyncCryptographer(username, friendlyBase32SyncKey);
-        CryptoStatusBundle result = cryptographer.generateCryptoKeysWBOPayload();
-
-        // Check result status and get generated keys
-        assertEquals(CryptoStatus.OK, result.getStatus());
-        KeyBundle createdKeys = cryptographer.getKeys();
-
-        // Decrypt what we created to see the keys as they were stored in the json
-        ExtendedJSONObject in = new ExtendedJSONObject();
-        in.put("payload", result.getJson());
-        in.put("id", "keys");
-        result = cryptographer.decryptWBO(in.toJSONString());
-
-        // Check result and get decrypted keys
-        KeyBundle decryptedKeys = cryptographer.getKeys();
-        assertEquals(CryptoStatus.OK, result.getStatus());
-
-        // Compare decrypted keys to the keys that were set upon creation
-        boolean equal = Arrays.equals(createdKeys.getEncryptionKey(), decryptedKeys.getEncryptionKey());
-        assertEquals(true, equal);
-        equal = Arrays.equals(createdKeys.getHMACKey(), decryptedKeys.getHMACKey());
-        assertEquals(true, equal);
-    }
 }
