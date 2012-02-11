83f9b302b0730378df73d8bd006ce9dd3e042e5e
Nick Alexander <nalexander@mozilla.com>
2012-02-10 21:17:08 -0800

    Update license blocks.

--- test/src/org/mozilla/android/sync/test/helpers/ExpectFinishDelegate.java
+++ test/src/org/mozilla/android/sync/test/helpers/ExpectFinishDelegate.java
@@ -1,3 +1,6 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
 package org.mozilla.android.sync.test.helpers;
 
 import org.mozilla.gecko.sync.repositories.RepositorySession;
