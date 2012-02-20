c74d6c381620fbf15ffa75f50e57437565db5b0a
Nick Alexander <nalexander@mozilla.com>
2012-02-20 15:40:20 -0800

    Bug 709310 - truth in naming: move tests from TestBackoff to TestGlobalSession, since they test GlobalSession.

--- src/test/java/org/mozilla/android/sync/net/test/TestGlobalSession.java
+++ src/test/java/org/mozilla/android/sync/net/test/TestGlobalSession.java
@@ -78,7 +78,91 @@ public class TestGlobalSession {
   }
 
   /**
-   * Test that handleHTTPError does in fact backoff.
+   * Test that interpretHTTPFailure calls requestBackoff if
+   * X-Weave-Backoff is present.
+   */
+  @Test
+  public void testBackoffCalledIfBackoffHeaderPresent() {
+    try {
+      final MockGlobalSessionCallback callback = new MockGlobalSessionCallback();
+      final GlobalSession session = new MockGlobalSession(TEST_CLUSTER_URL, TEST_USERNAME, TEST_PASSWORD,
+        new KeyBundle(TEST_USERNAME, TEST_SYNC_KEY), callback);
+
+      final HttpResponse response = new BasicHttpResponse(
+        new BasicStatusLine(new ProtocolVersion("HTTP", 1, 1), 200, "OK"));
+      response.addHeader("X-Weave-Backoff", Long.toString(TEST_BACKOFF_IN_SECONDS)); // Backoff given in seconds.
+
+      session.interpretHTTPFailure(new SyncStorageResponse(response)); // This is synchronous...
+
+      assertEquals(false, callback.calledSuccess); // ... so we can test immediately.
+      assertEquals(false, callback.calledError);
+      assertEquals(false, callback.calledAborted);
+      assertEquals(true,  callback.calledRequestBackoff);
+      assertEquals(TEST_BACKOFF_IN_SECONDS * 1000, callback.weaveBackoff); // Backoff returned in milliseconds.
+    } catch (Exception e) {
+      e.printStackTrace();
+      fail("Got exception.");
+    }
+  }
+
+  /**
+   * Test that interpretHTTPFailure does not call requestBackoff if
+   * X-Weave-Backoff is not present.
+   */
+  @Test
+  public void testBackoffNotCalledIfBackoffHeaderNotPresent() {
+    try {
+      final MockGlobalSessionCallback callback = new MockGlobalSessionCallback();
+      final GlobalSession session = new MockGlobalSession(TEST_CLUSTER_URL, TEST_USERNAME, TEST_PASSWORD,
+        new KeyBundle(TEST_USERNAME, TEST_SYNC_KEY), callback);
+
+      final HttpResponse response = new BasicHttpResponse(
+  new BasicStatusLine(new ProtocolVersion("HTTP", 1, 1), 200, "OK"));
+
+      session.interpretHTTPFailure(new SyncStorageResponse(response)); // This is synchronous...
+
+      assertEquals(false, callback.calledSuccess); // ... so we can test immediately.
+      assertEquals(false, callback.calledError);
+      assertEquals(false, callback.calledAborted);
+      assertEquals(false, callback.calledRequestBackoff);
+    } catch (Exception e) {
+      e.printStackTrace();
+      fail("Got exception.");
+    }
+  }
+
+  /**
+   * Test that interpretHTTPFailure calls requestBackoff with the
+   * largest specified value if X-Weave-Backoff and Retry-After are
+   * present.
+   */
+  @Test
+  public void testBackoffCalledIfMultipleBackoffHeadersPresent() {
+    try {
+      final MockGlobalSessionCallback callback = new MockGlobalSessionCallback();
+      final GlobalSession session = new MockGlobalSession(TEST_CLUSTER_URL, TEST_USERNAME, TEST_PASSWORD,
+        new KeyBundle(TEST_USERNAME, TEST_SYNC_KEY), callback);
+
+      final HttpResponse response = new BasicHttpResponse(
+        new BasicStatusLine(new ProtocolVersion("HTTP", 1, 1), 200, "OK"));
+      response.addHeader("Retry-After", Long.toString(TEST_BACKOFF_IN_SECONDS)); // Backoff given in seconds.
+      response.addHeader("X-Weave-Backoff", Long.toString(TEST_BACKOFF_IN_SECONDS + 1)); // If we now add a second header, the larger should be returned.
+
+      session.interpretHTTPFailure(new SyncStorageResponse(response)); // This is synchronous...
+
+      assertEquals(false, callback.calledSuccess); // ... so we can test immediately.
+      assertEquals(false, callback.calledError);
+      assertEquals(false, callback.calledAborted);
+      assertEquals(true,  callback.calledRequestBackoff);
+      assertEquals((TEST_BACKOFF_IN_SECONDS + 1) * 1000, callback.weaveBackoff); // Backoff returned in milliseconds.
+    } catch (Exception e) {
+      e.printStackTrace();
+      fail("Got exception.");
+    }
+  }
+
+  /**
+   * Test that abort does in fact backoff.
    */
   @Test
   public void testBackoffCalledByHandleHTTPError() {
