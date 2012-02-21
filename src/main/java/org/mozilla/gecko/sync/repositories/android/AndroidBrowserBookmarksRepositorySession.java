097e02277662184d2683fac1fbac4a3692ea4996
Richard Newman <rnewman@twinql.com>
2012-02-20 23:43:24 -0800

    Always use remote children array.

--- src/main/java/org/mozilla/gecko/sync/repositories/android/AndroidBrowserBookmarksRepositorySession.java
+++ src/main/java/org/mozilla/gecko/sync/repositories/android/AndroidBrowserBookmarksRepositorySession.java
@@ -536,6 +536,10 @@ public class AndroidBrowserBookmarksRepositorySession extends AndroidBrowserRepo
                                                                         lastRemoteRetrieval,
                                                                         lastLocalRetrieval);
 
+    // For now we *always* use the remote record's children array as a starting point.
+    // We won't write it into the database yet; we'll record it and process as we go.
+    reconciled.children = ((BookmarkRecord) remoteRecord).children;
+
     // null:   local wins, or equal: nothing to be done.
     // record: the most recent record, with remote GUID and local Android ID.
     // Side-effect: if the remote record has won, track it to avoid upload.
@@ -546,6 +550,12 @@ public class AndroidBrowserBookmarksRepositorySession extends AndroidBrowserRepo
     // - if the child array changed, un-track the record. (TODO: this should
     //   be unnecessary if we track with modified times…)
     // - update parenting maps. This should get taken care of during insertion.
+    /*
+     *
+     *  Don't do this here. We don't even want to bother computing local children
+     *  until we've dealt with every record.
+     *
+     *
     if (reconciled == null) {
       return reconciled;
     }
@@ -564,6 +574,7 @@ public class AndroidBrowserBookmarksRepositorySession extends AndroidBrowserRepo
       // Child array changed, so we do want to upload it after all!
       this.untrackRecord(reconciled);
     }
+    */
     return reconciled;
   }
 
