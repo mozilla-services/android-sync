cf2db3baa3ddfc142a075a04dab86754ae6e9445
Nick Alexander <nalexander@mozilla.com>
2012-03-07 13:16:54 -0800

    Bug 709402: handle more exceptions on main thread; make DefaultFetchDelegate verify that all expected records were actually fetched.

--- test/src/org/mozilla/android/sync/test/helpers/ExpectFinishFailDelegate.java
+++ test/src/org/mozilla/android/sync/test/helpers/ExpectFinishFailDelegate.java
@@ -1,14 +1,12 @@
 package org.mozilla.android.sync.test.helpers;
 
-import static junit.framework.Assert.fail;
-
 import org.mozilla.gecko.sync.repositories.InvalidSessionTransitionException;
 
 public class ExpectFinishFailDelegate extends DefaultFinishDelegate {
   @Override
   public void onFinishFailed(Exception ex) {
-    if (ex.getClass() != InvalidSessionTransitionException.class) {
-      fail("Wrong exception received");
+    if (!(ex instanceof InvalidSessionTransitionException)) {
+      performNotify("Expected InvalidSessionTransititionException but got ", ex);
     }
   }
 }
