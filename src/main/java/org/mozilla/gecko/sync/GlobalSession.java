080e809eab6f34bc09e01575c4ab211af14db28e
Nick Alexander <nalexander@mozilla.com>
2012-03-12 13:13:15 -0700

    Bug 709402: add AndroidBrowserFormHistoryServerSyncStage.

--- src/main/java/org/mozilla/gecko/sync/GlobalSession.java
+++ src/main/java/org/mozilla/gecko/sync/GlobalSession.java
@@ -27,6 +27,7 @@ import org.mozilla.gecko.sync.net.SyncStorageRequest;
 import org.mozilla.gecko.sync.net.SyncStorageRequestDelegate;
 import org.mozilla.gecko.sync.net.SyncStorageResponse;
 import org.mozilla.gecko.sync.stage.AndroidBrowserBookmarksServerSyncStage;
+import org.mozilla.gecko.sync.stage.AndroidBrowserFormHistoryServerSyncStage;
 import org.mozilla.gecko.sync.stage.AndroidBrowserHistoryServerSyncStage;
 import org.mozilla.gecko.sync.stage.CheckPreconditionsStage;
 import org.mozilla.gecko.sync.stage.CompletedStage;
@@ -171,6 +172,7 @@ public class GlobalSession implements CredentialsSource, PrefsSource {
     stages.put(Stage.syncTabs,                new FennecTabsServerSyncStage());
     stages.put(Stage.syncBookmarks,           new AndroidBrowserBookmarksServerSyncStage());
     stages.put(Stage.syncHistory,             new AndroidBrowserHistoryServerSyncStage());
+    stages.put(Stage.syncFormHistory,         new AndroidBrowserFormHistoryServerSyncStage());
     stages.put(Stage.completed,               new CompletedStage());
   }
 
