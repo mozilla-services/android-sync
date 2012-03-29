8081486e906df7c97c1c05191a39290d48f5c8de
Marina Samuel <marina.samuel25@gmail.com>
2012-03-29 15:01:08 -0700

    Bug 740247 Part 3 - Reset local timestamp on download.

--- src/main/java/org/mozilla/gecko/sync/stage/SyncClientsEngineStage.java
+++ src/main/java/org/mozilla/gecko/sync/stage/SyncClientsEngineStage.java
@@ -66,6 +66,7 @@ public class SyncClientsEngineStage implements GlobalSyncStage {
 
     // We use this on each WBO, so lift it out.
     final ClientsDataDelegate clientsDelegate = session.getClientsDelegate();
+    boolean localAccountGUIDDownloaded = false;
 
     @Override
     public String credentials() {
@@ -81,6 +82,14 @@ public class SyncClientsEngineStage implements GlobalSyncStage {
     @Override
     public void handleRequestSuccess(SyncStorageResponse response) {
       BaseResource.consumeEntity(response); // We don't need the response at all.
+
+      // If we successfully downloaded all records but ours was not one of them
+      // then reset the timestamp.
+      if (!localAccountGUIDDownloaded) {
+        session.config.persistServerClientRecordTimestamp(0);
+      }
+      localAccountGUIDDownloaded = false;
+
       final int clientsCount;
       try {
         clientsCount = db.clientsCount();
@@ -102,6 +111,8 @@ public class SyncClientsEngineStage implements GlobalSyncStage {
     @Override
     public void handleRequestFailure(SyncStorageResponse response) {
       BaseResource.consumeEntity(response); // We don't need the response at all, and any exception handling shouldn't need the response body.
+      localAccountGUIDDownloaded = false;
+
       try {
         Logger.info(LOG_TAG, "Client upload failed. Aborting sync.");
         session.abort(new HTTPFailureException(response), "Client download failed.");
@@ -113,6 +124,7 @@ public class SyncClientsEngineStage implements GlobalSyncStage {
 
     @Override
     public void handleRequestError(Exception ex) {
+      localAccountGUIDDownloaded = false;
       try {
         Logger.info(LOG_TAG, "Client upload error. Aborting sync.");
         session.abort(ex, "Failure fetching client record.");
@@ -128,6 +140,7 @@ public class SyncClientsEngineStage implements GlobalSyncStage {
       try {
         r = (ClientRecord) factory.createRecord(record.decrypt());
         if (clientsDelegate.isLocalGUID(r.guid)) {
+          localAccountGUIDDownloaded = true;
           // Oh hey! Our record is on the server. This is the authoritative
           // server timestamp, so let's hang on to it to decide whether we
           // need to upload.
