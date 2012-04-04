54d42089fff4188efaad95b33a60c922275096e5
Nick Alexander <nalexander@mozilla.com>
2012-04-04 15:22:58 -0700

    Make check_head_headers.sh check uncommitted files.

--- check_head_headers.sh
+++ check_head_headers.sh
@@ -2,6 +2,12 @@
 
 # Verify that HEAD headers are up to date.
 
+# first files commited but not merged to develop.
 python "tools/check_headers.py" -l "package" -f "tools/COPYRIGHT_MPL" -x "test" "$@" `git diff --name-only develop HEAD | grep ^src/main/java/org/mozilla/`
-python "tools/check_headers.py" -l "package" -f "tools/COPYRIGHT_PD" "$@" `git diff --name-only develop HEAD | grep ^src/test/java/org/mozilla`
-python "tools/check_headers.py" -l "package" -f "tools/COPYRIGHT_PD" "$@" `git diff --name-only develop HEAD | grep ^test/src/org/mozilla`
+python "tools/check_headers.py" -l "package" -f "tools/COPYRIGHT_PD" "$@"            `git diff --name-only develop HEAD | grep ^src/test/java/org/mozilla`
+python "tools/check_headers.py" -l "package" -f "tools/COPYRIGHT_PD" "$@"            `git diff --name-only develop HEAD | grep ^test/src/org/mozilla`
+
+# second files we are modifying.
+python "tools/check_headers.py" -l "package" -f "tools/COPYRIGHT_MPL" -x "test" "$@" `git ls-files --modified | grep ^src/main/java/org/mozilla/`
+python "tools/check_headers.py" -l "package" -f "tools/COPYRIGHT_PD" "$@"            `git ls-files --modified | grep ^src/test/java/org/mozilla`
+python "tools/check_headers.py" -l "package" -f "tools/COPYRIGHT_PD" "$@"            `git ls-files --modified | grep ^test/src/org/mozilla`
