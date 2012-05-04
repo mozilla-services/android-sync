bdbc8c4d8442f103216c151cfd9b421ac9c9f515
Nick Alexander <nalexander@mozilla.com>
2012-05-03 17:37:06 -0700

    Bug 709311 - Part 2: check remote engine settings against local settings, resetting or wiping the server as necessary.

--- src/main/java/org/mozilla/gecko/sync/stage/ServerSyncStage.java
+++ src/main/java/org/mozilla/gecko/sync/stage/ServerSyncStage.java
@@ -9,6 +9,7 @@ import java.net.URISyntaxException;
 import java.util.concurrent.ExecutorService;
 
 import org.json.simple.parser.ParseException;
+import org.mozilla.gecko.sync.CredentialsSource;
 import org.mozilla.gecko.sync.EngineSettings;
 import org.mozilla.gecko.sync.GlobalSession;
 import org.mozilla.gecko.sync.HTTPFailureException;
@@ -17,9 +18,13 @@ import org.mozilla.gecko.sync.MetaGlobalException;
 import org.mozilla.gecko.sync.NoCollectionKeysSetException;
 import org.mozilla.gecko.sync.NonObjectJSONException;
 import org.mozilla.gecko.sync.SynchronizerConfiguration;
-import org.mozilla.gecko.sync.Utils;
 import org.mozilla.gecko.sync.crypto.KeyBundle;
+import org.mozilla.gecko.sync.delegates.WipeServerDelegate;
 import org.mozilla.gecko.sync.middleware.Crypto5MiddlewareRepository;
+import org.mozilla.gecko.sync.net.BaseResource;
+import org.mozilla.gecko.sync.net.SyncStorageRequest;
+import org.mozilla.gecko.sync.net.SyncStorageRequestDelegate;
+import org.mozilla.gecko.sync.net.SyncStorageResponse;
 import org.mozilla.gecko.sync.repositories.InactiveSessionException;
 import org.mozilla.gecko.sync.repositories.InvalidSessionTransitionException;
 import org.mozilla.gecko.sync.repositories.RecordFactory;
@@ -73,7 +78,7 @@ public abstract class ServerSyncStage implements
       // Fall through; null engineSettings will pass below.
     }
 
-    // Catch the subclasses of MetaGlobalException to trigger various resets and wipes.
+    // We catch the subclasses of MetaGlobalException to trigger various resets and wipes in execute().
     return session.engineIsEnabled(this.getEngineName(), engineSettings);
   }
 
@@ -85,9 +90,8 @@ public abstract class ServerSyncStage implements
     }
 
     SynchronizerConfiguration config = this.getConfig();
-
-    if (config == null || config.syncID == null) {
-      return new EngineSettings(Utils.generateGuid(), version.intValue());
+    if (config == null) {
+      return new EngineSettings(null, version.intValue());
     }
     return new EngineSettings(config.syncID, version.intValue());
   }
@@ -137,15 +141,24 @@ public abstract class ServerSyncStage implements
     Synchronizer synchronizer = new Synchronizer();
     synchronizer.repositoryA = remote;
     synchronizer.repositoryB = this.getLocalRepository();
-
-    SynchronizerConfiguration config = this.getConfig();
-    synchronizer.load(config);
+    synchronizer.load(getConfig());
 
     return synchronizer;
   }
 
+  /**
+   * Reset timestamps.
+   */
   @Override
   public void resetLocal() {
+    resetLocal(null);
+  }
+
+  /**
+   * Reset timestamps and possibly set syncID.
+   * @param syncID if non-null, new syncID to persist.
+   */
+  public void resetLocal(String syncID) {
     // Clear both timestamps.
     SynchronizerConfiguration config;
     try {
@@ -155,10 +168,14 @@ public abstract class ServerSyncStage implements
       return;
     }
 
+    if (syncID != null) {
+      config.syncID = syncID;
+      Logger.info(LOG_TAG, "Setting syncID for " + this + " to '" + syncID + "'.");
+    }
     config.localBundle.setTimestamp(0L);
     config.remoteBundle.setTimestamp(0L);
-    Logger.info(LOG_TAG, "Reset timestamps for " + this);
     persistConfig(config);
+    Logger.info(LOG_TAG, "Reset timestamps for " + this);
   }
 
   // Not thread-safe. Use with caution.
@@ -178,8 +195,8 @@ public abstract class ServerSyncStage implements
   /**
    * Synchronously wipe this stage by instantiating a local repository session
    * and wiping that.
-   *
-   * Logs and rethrows an exception on failure.
+   * <p>
+   * Logs and re-throws an exception on failure.
    */
   @Override
   public void wipeLocal() throws Exception {
@@ -312,6 +329,105 @@ public abstract class ServerSyncStage implements
     Logger.info(LOG_TAG, "Wiping stage complete.");
   }
 
+  /**
+   * Asynchronously wipe collection on server.
+   */
+  protected void wipeServer(final CredentialsSource credentials, final WipeServerDelegate wipeDelegate) {
+    SyncStorageRequest request;
+
+    try {
+      request = new SyncStorageRequest(session.config.collectionURI(getCollection()));
+    } catch (URISyntaxException ex) {
+      Logger.warn(LOG_TAG, "Invalid URI in wipeServer.");
+      wipeDelegate.onWipeFailed(ex);
+      return;
+    }
+
+    request.delegate = new SyncStorageRequestDelegate() {
+
+      @Override
+      public String ifUnmodifiedSince() {
+        return null;
+      }
+
+      @Override
+      public void handleRequestSuccess(SyncStorageResponse response) {
+        BaseResource.consumeEntity(response);
+        resetLocal();
+        wipeDelegate.onWiped(response.normalizedWeaveTimestamp());
+      }
+
+      @Override
+      public void handleRequestFailure(SyncStorageResponse response) {
+        Logger.warn(LOG_TAG, "Got request failure " + response.getStatusCode() + " in wipeServer.");
+        // Process HTTP failures here to pick up backoffs, etc.
+        session.interpretHTTPFailure(response.httpResponse());
+        BaseResource.consumeEntity(response); // The exception thrown should not need the body of the response.
+        wipeDelegate.onWipeFailed(new HTTPFailureException(response));
+      }
+
+      @Override
+      public void handleRequestError(Exception ex) {
+        Logger.warn(LOG_TAG, "Got exception in wipeServer.", ex);
+        wipeDelegate.onWipeFailed(ex);
+      }
+
+      @Override
+      public String credentials() {
+        return credentials.credentials();
+      }
+    };
+
+    request.delete();
+  }
+
+  /**
+   * Synchronously wipe the server.
+   * <p>
+   * Logs and re-throws an exception on failure.
+   */
+  public void wipeServer() throws Exception {
+    final WipeWaiter monitor = new WipeWaiter();
+
+    final Runnable doWipe = new Runnable() {
+      @Override
+      public void run() {
+        wipeServer(session, new WipeServerDelegate() {
+          @Override
+          public void onWiped(long timestamp) {
+            synchronized (monitor) {
+              monitor.notify();
+            }
+          }
+
+          @Override
+          public void onWipeFailed(Exception e) {
+            synchronized (monitor) {
+              monitor.notify(e, false);
+            }
+          }
+        });
+      }
+    };
+
+    final Thread wiping = new Thread(doWipe);
+    synchronized (monitor) {
+      wiping.start();
+      try {
+        monitor.wait();
+      } catch (InterruptedException e) {
+        Logger.error(LOG_TAG, "Server wipe interrupted.");
+      }
+    }
+
+    if (!monitor.wipeSucceeded) {
+      Logger.error(LOG_TAG, "Failed to wipe server.");
+      throw monitor.error;
+    }
+
+    Logger.info(LOG_TAG, "Wiping server complete.");
+  }
+
   @Override
   public void execute() throws NoSuchStageException {
     final String name = getEngineName();
@@ -323,11 +439,31 @@ public abstract class ServerSyncStage implements
         session.advance();
         return;
       }
+    } catch (MetaGlobalException.MetaGlobalMalformedSyncIDException e) {
+      // Bad engine syncID. This should never happen. Wipe the server.
+      try {
+        wipeServer();
+        Logger.info(LOG_TAG, "Wiped server after malformed engine sync ID found in meta/global.");
+      } catch (Exception ex) {
+        session.abort(ex, "Failed to wipe server after malformed engine sync ID found in meta/global.");
+      }
+    } catch (MetaGlobalException.MetaGlobalMalformedVersionException e) {
+      // Bad engine version. This should never happen. Wipe the server.
+      try {
+        wipeServer();
+        Logger.info(LOG_TAG, "Wiped server after malformed engine version found in meta/global.");
+      } catch (Exception ex) {
+        session.abort(ex, "Failed to wipe server after malformed engine version found in meta/global.");
+      }
+    } catch (MetaGlobalException.MetaGlobalStaleClientSyncIDException e) {
+      // Our syncID is wrong. Reset client and take the server syncID.
+      Logger.warn(LOG_TAG, "Remote engine syncID different from local engine syncID:" +
+          "resetting local engine and assuming remote engine syncID.");
+      this.resetLocal(e.serverSyncID);
     } catch (MetaGlobalException e) {
       session.abort(e, "Inappropriate meta/global; refusing to execute " + name + " stage.");
       return;
     }
-
 
     Synchronizer synchronizer;
     try {
@@ -358,9 +494,9 @@ public abstract class ServerSyncStage implements
   public void onSynchronized(Synchronizer synchronizer) {
     Logger.debug(LOG_TAG, "onSynchronized.");
 
-    SynchronizerConfiguration synchronizerConfiguration = synchronizer.save();
-    if (synchronizerConfiguration != null) {
-      persistConfig(synchronizerConfiguration);
+    SynchronizerConfiguration newConfig = synchronizer.save();
+    if (newConfig != null) {
+      persistConfig(newConfig);
     } else {
       Logger.warn(LOG_TAG, "Didn't get configuration from synchronizer after success");
     }
