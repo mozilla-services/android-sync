141adcd07faade102184e698fa290bc1a5524277
Nick Alexander <nalexander@mozilla.com>
2012-05-22 17:03:04 -0700

    Bug 736393 - make Server11RepositorySession report successes and failures, and not try to upload if a previous upload failed.

--- src/test/java/org/mozilla/android/sync/test/TestServer11RepositorySession.java
+++ src/test/java/org/mozilla/android/sync/test/TestServer11RepositorySession.java
@@ -3,6 +3,8 @@
 
 package org.mozilla.android.sync.test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
@@ -10,24 +12,35 @@ import java.net.URI;
 import java.net.URISyntaxException;
 
 import org.junit.Test;
+import org.mozilla.android.sync.test.SynchronizerHelpers.TrackingWBORepository;
 import org.mozilla.android.sync.test.helpers.BaseTestStorageRequestDelegate;
 import org.mozilla.android.sync.test.helpers.HTTPServerTestHelper;
 import org.mozilla.android.sync.test.helpers.MockRecord;
 import org.mozilla.android.sync.test.helpers.MockServer;
+import org.mozilla.gecko.sync.CredentialsSource;
+import org.mozilla.gecko.sync.Logger;
 import org.mozilla.gecko.sync.Utils;
+import org.mozilla.gecko.sync.crypto.KeyBundle;
+import org.mozilla.gecko.sync.middleware.Crypto5MiddlewareRepository;
 import org.mozilla.gecko.sync.net.BaseResource;
 import org.mozilla.gecko.sync.net.SyncStorageRecordRequest;
 import org.mozilla.gecko.sync.net.SyncStorageResponse;
+import org.mozilla.gecko.sync.repositories.FetchFailedException;
 import org.mozilla.gecko.sync.repositories.Repository;
+import org.mozilla.gecko.sync.repositories.Server11Repository;
 import org.mozilla.gecko.sync.repositories.Server11RepositorySession;
+import org.mozilla.gecko.sync.repositories.StoreFailedException;
+import org.mozilla.gecko.sync.repositories.domain.BookmarkRecord;
+import org.mozilla.gecko.sync.repositories.domain.BookmarkRecordFactory;
 import org.mozilla.gecko.sync.repositories.domain.Record;
+import org.mozilla.gecko.sync.synchronizer.Synchronizer;
 import org.simpleframework.http.ContentType;
 import org.simpleframework.http.Request;
 import org.simpleframework.http.Response;
 
 import ch.boye.httpclientandroidlib.HttpEntity;
 
-public class TestServer11RepositorySession {
+public class TestServer11RepositorySession implements CredentialsSource {
 
   public class POSTMockServer extends MockServer {
     @Override
@@ -45,13 +58,18 @@ public class TestServer11RepositorySession {
   }
 
   private static final int    TEST_PORT   = 15325;
-  private static final String TEST_SERVER = "http://localhost:" + TEST_PORT;
-  static final String LOCAL_REQUEST_URL   = TEST_SERVER + "/1.1/n6ec3u5bee3tixzp2asys7bs6fve4jfw/storage/bookmarks";
+  private static final String TEST_SERVER = "http://localhost:" + TEST_PORT + "/";
+  static final String LOCAL_REQUEST_URL   = TEST_SERVER + "1.1/n6ec3u5bee3tixzp2asys7bs6fve4jfw/storage/bookmarks";
 
   // Corresponds to rnewman+atest1@mozilla.com, local.
   static final String USERNAME          = "n6ec3u5bee3tixzp2asys7bs6fve4jfw";
   static final String USER_PASS         = "n6ec3u5bee3tixzp2asys7bs6fve4jfw:password";
   static final String SYNC_KEY          = "eh7ppnb82iwr5kt3z3uyi5vr44";
+
+  @Override
+  public String credentials() {
+    return USER_PASS;
+  }
 
   private HTTPServerTestHelper data     = new HTTPServerTestHelper(TEST_PORT);
 
@@ -62,7 +80,7 @@ public class TestServer11RepositorySession {
 
     public RecordUploadRunnable getRecordUploadRunnable() {
       // TODO: implement upload delegate in the class, too!
-      return new RecordUploadRunnable(null, recordsBuffer, byteCount);
+      return new RecordUploadRunnable(null, recordsBuffer, recordGuidsBuffer, byteCount);
     }
 
     public void enqueueRecord(Record r) {
@@ -102,5 +120,77 @@ public class TestServer11RepositorySession {
     delegate._credentials = USER_PASS;
     r.delegate = delegate;
     r.post(session.getEntity());
+  }
+
+  protected TrackingWBORepository getLocal(int numRecords) {
+    final TrackingWBORepository local = new TrackingWBORepository();
+    for (int i = 0; i < numRecords; i++) {
+      BookmarkRecord outbound = new BookmarkRecord("outboundFail" + i, "bookmarks", 1, false);
+      local.wbos.put(outbound.guid, outbound);
+    }
+    return local;
+  }
+
+  protected Exception doSynchronize(MockServer server) throws Exception {
+    final String COLLECTION = "test";
+
+    final TrackingWBORepository local = getLocal(100);
+    final Server11Repository remote = new Server11Repository(TEST_SERVER, USERNAME, COLLECTION, this);
+    KeyBundle collectionKey = new KeyBundle(USERNAME, SYNC_KEY);
+    Crypto5MiddlewareRepository cryptoRepo = new Crypto5MiddlewareRepository(remote, collectionKey);
+    cryptoRepo.recordFactory = new BookmarkRecordFactory();
+
+    final Synchronizer synchronizer = new Synchronizer();
+    synchronizer.repositoryA = cryptoRepo;
+    synchronizer.repositoryB = local;
+
+    data.startHTTPServer(server);
+    try {
+      Exception e = TestSynchronizer.doSynchronize(synchronizer);
+      return e;
+    } finally {
+      data.stopHTTPServer();
+    }
+  }
+
+  @Test
+  public void testFetchFailure() throws Exception {
+    Logger.LOG_TO_STDOUT = true;
+
+    MockServer server = new MockServer(404, "error");
+    Exception e = doSynchronize(server);
+    assertNotNull(e);
+    assertEquals(FetchFailedException.class, e.getClass());
+  }
+
+  @Test
+  public void testStorePostSuccessWithFailingRecords() throws Exception {
+    Logger.LOG_TO_STDOUT = true;
+
+    MockServer server = new MockServer(200, "{ modified: \" + " + Utils.millisecondsToDecimalSeconds(System.currentTimeMillis()) + ", " +
+        "success: []," +
+        "failed: { outboundFail2: [] } }");
+    Exception e = doSynchronize(server);
+    assertNotNull(e);
+    assertEquals(StoreFailedException.class, e.getClass());
+  }
+
+  @Test
+  public void testStorePostFailure() throws Exception {
+    Logger.LOG_TO_STDOUT = true;
+
+    MockServer server = new MockServer() {
+      public void handle(Request request, Response response) {
+        if (request.getMethod().equals("POST")) {
+          this.handle(request, response, 404, "missing");
+        }
+        this.handle(request, response, 200, "success");
+        return;
+      }
+    };
+
+    Exception e = doSynchronize(server);
+    assertNotNull(e);
+    assertEquals(StoreFailedException.class, e.getClass());
   }
 }
