ebf9c9873f438b4c84c186501659b7b4f00f4786
Nick Alexander <nalexander@mozilla.com>
2012-05-30 15:10:09 -0700

    Bug 736393 - address review comments in Server11RepositorySession.

---
+++ src/main/java/org/mozilla/gecko/sync/Server11RecordPostFailedException.java
@@ -0,0 +1,8 @@
+package org.mozilla.gecko.sync;
+
+/**
+ * The server rejected a record in its "failure" array.
+ */
+public class Server11RecordPostFailedException extends SyncException {
+  private static final long serialVersionUID = -8517471217486190314L;
+}
