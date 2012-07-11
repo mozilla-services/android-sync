95af5dbcff2fa25ed78578c05ea40268ee8d90d0
Nick Alexander <nalexander@mozilla.com>
2012-07-11 14:45:10 -0700

    Abort on mvn errors in fennec-code-drop.sh. r=trivial

--- fennec-code-drop.sh
+++ fennec-code-drop.sh
@@ -23,12 +23,14 @@ fi
 echo "Preprocessing."
 ./preprocess.sh
 
+mvn clean compile || { echo 'mvn clean compile failed'; exit 1; }
+
 if [ "$1" = "notests" ]
 then
   echo "Not running tests."
 else
   echo "Running tests."
-  mvn clean test
+  mvn test || { echo 'mvn test failed'; exit 1; }
 fi
 shift
 
