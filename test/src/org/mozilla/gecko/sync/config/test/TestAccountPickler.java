126d7fa9641af17b89f5812caa5fc9d2b2fc8a86
Nick Alexander <nalexander@mozilla.com>
2012-07-12 12:06:18 -0700

    Bug 773354 - Make TestAccountPickler only delete test Android Account objects; update test that passed for the wrong reason.
    TestAccountPickler.testUnpickleAccountAlreadyExists passed because the
    second Account created had the same parameters.  It was intended to
    verify that if a Sync account already existed, we wouldn't unpickle
    another one.  We don't (phew!), but that logic is in
    SyncAccounts.syncAccountsExist.  I updated the test to verify the
    correct flow doesn't create a second Account.

--- test/src/org/mozilla/gecko/sync/config/test/TestAccountPickler.java
+++ test/src/org/mozilla/gecko/sync/config/test/TestAccountPickler.java
@@ -7,6 +7,8 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.mozilla.android.sync.test.AndroidSyncTestCase;
 import org.mozilla.gecko.db.BrowserContract;
@@ -14,6 +16,7 @@ import org.mozilla.gecko.sync.ExtendedJSONObject;
 import org.mozilla.gecko.sync.Utils;
 import org.mozilla.gecko.sync.config.AccountPickler;
 import org.mozilla.gecko.sync.setup.Constants;
+import org.mozilla.gecko.sync.setup.SyncAccounts;
 import org.mozilla.gecko.sync.setup.SyncAccounts.SyncAccountParameters;
 import org.mozilla.gecko.sync.setup.test.TestSyncAccounts;
 
@@ -26,26 +29,53 @@ public class TestAccountPickler extends AndroidSyncTestCase {
   public static final String TEST_FILENAME = "test.json";
   public static final String TEST_ACCOUNTTYPE = Constants.ACCOUNTTYPE_SYNC;
 
+  // Test account names must start with TEST_USERNAME in order to be recognized
+  // as test accounts and deleted in tearDown.
   public static final String TEST_USERNAME   = "testAccount@mozilla.com";
+  public static final String TEST_USERNAME2  = TEST_USERNAME + "2";
+
   public static final String TEST_SYNCKEY    = "testSyncKey";
   public static final String TEST_PASSWORD   = "testPassword";
-  public static final String TEST_SERVERURL  = "test.server.url/";
+  public static final String TEST_SERVER_URL = "test.server.url/";
   public static final String TEST_CLIENT_NAME = "testClientName";
   public static final String TEST_CLIENT_GUID = "testClientGuid";
 
   protected SyncAccountParameters params;
+  protected Context context;
+  protected AccountManager accountManager;
+  protected int numAccounts;
 
   public void setUp() {
-    params = new SyncAccountParameters(getApplicationContext(), null,
-        TEST_USERNAME, TEST_SYNCKEY, TEST_PASSWORD, TEST_SERVERURL, null, TEST_CLIENT_NAME, TEST_CLIENT_GUID);
+    context = getApplicationContext();
+    accountManager = AccountManager.get(context);
+    params = new SyncAccountParameters(context, accountManager,
+        TEST_USERNAME, TEST_SYNCKEY, TEST_PASSWORD, TEST_SERVER_URL, null, TEST_CLIENT_NAME, TEST_CLIENT_GUID);
+
+    numAccounts = accountManager.getAccountsByType(TEST_ACCOUNTTYPE).length;
+  }
+
+  public static List<Account> getTestAccounts(final AccountManager accountManager) {
+    final List<Account> testAccounts = new ArrayList<Account>();
+
+    final Account[] accounts = accountManager.getAccountsByType(TEST_ACCOUNTTYPE);
+    for (Account account : accounts) {
+      if (account.name.startsWith(TEST_USERNAME)) {
+        testAccounts.add(account);
+      }
+    }
+
+    return testAccounts;
+  }
+
+  public void deleteTestAccounts() {
+    for (Account account : getTestAccounts(accountManager)) {
+      TestSyncAccounts.deleteAccount(this, accountManager, account);
+    }
   }
 
   public void tearDown() {
-    final AccountManager accountManager = AccountManager.get(getApplicationContext());
-    final Account[] accounts = accountManager.getAccountsByType(TEST_ACCOUNTTYPE);
-    for (Account account : accounts) {
-      TestSyncAccounts.deleteAccount(this, accountManager, account);
-    }
+    deleteTestAccounts();
+    assertEquals(numAccounts, accountManager.getAccountsByType(TEST_ACCOUNTTYPE).length);
   }
 
   public static void assertFileNotPresent(final Context context, final String filename) throws Exception {
@@ -64,8 +94,6 @@ public class TestAccountPickler extends AndroidSyncTestCase {
   }
 
   public void testPersist() throws Exception {
-    final Context context = getApplicationContext();
-
     context.deleteFile(TEST_FILENAME);
     assertFileNotPresent(context, TEST_FILENAME);
 
@@ -77,7 +105,7 @@ public class TestAccountPickler extends AndroidSyncTestCase {
     final ExtendedJSONObject o = ExtendedJSONObject.parseJSONObject(s);
     assertEquals(TEST_USERNAME,  o.getString(Constants.JSON_KEY_ACCOUNT));
     assertEquals(TEST_PASSWORD,  o.getString(Constants.JSON_KEY_PASSWORD));
-    assertEquals(TEST_SERVERURL, o.getString(Constants.JSON_KEY_SERVER));
+    assertEquals(TEST_SERVER_URL, o.getString(Constants.JSON_KEY_SERVER));
     assertEquals(TEST_SYNCKEY,   o.getString(Constants.JSON_KEY_SYNCKEY));
     assertNull(o.getString(Constants.JSON_KEY_CLUSTER));
     assertEquals(TEST_CLIENT_NAME, o.getString(Constants.JSON_KEY_CLIENT_NAME));
@@ -88,7 +116,6 @@ public class TestAccountPickler extends AndroidSyncTestCase {
   }
 
   public void testDeletePickle() throws Exception {
-    final Context context = getApplicationContext();
     AccountPickler.pickle(context, TEST_FILENAME, params, false);
 
     // Verify file is present.
@@ -101,21 +128,13 @@ public class TestAccountPickler extends AndroidSyncTestCase {
   }
 
   public Account deleteAccountsAndUnpickle(final Context context, final AccountManager accountManager, final String filename) {
-    Account[] accounts = accountManager.getAccountsByType(TEST_ACCOUNTTYPE);
-    for (Account account : accounts) {
-      TestSyncAccounts.deleteAccount(this, accountManager, account);
-    }
-
-    accounts = accountManager.getAccountsByType(TEST_ACCOUNTTYPE);
-    assertEquals(0, accounts.length);
+    deleteTestAccounts();
+    assertEquals(0, getTestAccounts(accountManager).size());
 
     return AccountPickler.unpickle(context, filename);
   }
 
   public void testUnpickleSuccess() throws Exception {
-    final Context context = getApplicationContext();
-    final AccountManager accountManager = AccountManager.get(context);
-
     AccountPickler.pickle(context, TEST_FILENAME, params, true);
 
     // Make sure we have no accounts hanging around.
@@ -123,11 +142,8 @@ public class TestAccountPickler extends AndroidSyncTestCase {
     assertNotNull(account);
 
     try {
-      Account[] accounts = accountManager.getAccountsByType(TEST_ACCOUNTTYPE);
-      assertEquals(1, accounts.length);
-
+      assertEquals(1, getTestAccounts(accountManager).size());
       assertTrue(ContentResolver.getSyncAutomatically(account, BrowserContract.AUTHORITY));
-
       assertEquals(account.name, TEST_USERNAME);
 
       // Verify Account parameters are in place.  Not testing clusterURL since it's stored in
@@ -139,7 +155,7 @@ public class TestAccountPickler extends AndroidSyncTestCase {
       final String clientGuid = accountManager.getUserData(account, Constants.ACCOUNT_GUID);
 
       assertEquals(TEST_PASSWORD, password);
-      assertEquals(TEST_SERVERURL, serverURL);
+      assertEquals(TEST_SERVER_URL, serverURL);
       assertEquals(TEST_SYNCKEY, syncKey);
       assertEquals(TEST_CLIENT_NAME, clientName);
       assertEquals(TEST_CLIENT_GUID, clientGuid);
@@ -149,9 +165,6 @@ public class TestAccountPickler extends AndroidSyncTestCase {
   }
 
   public void testUnpickleNoAutomatic() throws Exception {
-    final Context context = getApplicationContext();
-    final AccountManager accountManager = AccountManager.get(context);
-
     AccountPickler.pickle(context, TEST_FILENAME, params, false);
 
     // Make sure we have no accounts hanging around.
@@ -159,9 +172,7 @@ public class TestAccountPickler extends AndroidSyncTestCase {
     assertNotNull(account);
 
     try {
-      Account[] accounts = accountManager.getAccountsByType(TEST_ACCOUNTTYPE);
-      assertEquals(1, accounts.length);
-
+      assertEquals(1, getTestAccounts(accountManager).size());
       assertFalse(ContentResolver.getSyncAutomatically(account, BrowserContract.AUTHORITY));
     } finally {
       TestSyncAccounts.deleteAccount(this, accountManager, account);
@@ -169,9 +180,6 @@ public class TestAccountPickler extends AndroidSyncTestCase {
   }
 
   public void testUnpickleNoFile() {
-    final Context context = getApplicationContext();
-    final AccountManager accountManager = AccountManager.get(context);
-
     // Just in case file is hanging around.
     context.deleteFile(TEST_FILENAME);
 
@@ -180,9 +188,6 @@ public class TestAccountPickler extends AndroidSyncTestCase {
   }
 
   public void testUnpickleIncompleteUserData() throws Exception {
-    final Context context = getApplicationContext();
-    final AccountManager accountManager = AccountManager.get(context);
-
     final FileOutputStream fos = context.openFileOutput(TEST_FILENAME, Context.MODE_PRIVATE);
     final PrintStream ps = (new PrintStream(fos));
     ps.print("{}"); // Valid JSON, just missing everything.
@@ -194,9 +199,6 @@ public class TestAccountPickler extends AndroidSyncTestCase {
   }
 
   public void testUnpickleMalformedFile() throws Exception {
-    final Context context = getApplicationContext();
-    final AccountManager accountManager = AccountManager.get(context);
-
     final FileOutputStream fos = context.openFileOutput(TEST_FILENAME, Context.MODE_PRIVATE);
     final PrintStream ps = (new PrintStream(fos));
     ps.print("{1:!}"); // Not valid JSON.
@@ -208,24 +210,25 @@ public class TestAccountPickler extends AndroidSyncTestCase {
   }
 
   public void testUnpickleAccountAlreadyExists() {
-    final Context context = getApplicationContext();
-    final AccountManager accountManager = AccountManager.get(context);
-
     AccountPickler.pickle(context, TEST_FILENAME, params, false);
 
-    // Make sure we have no accounts hanging around.
+    // Make sure we have no test accounts hanging around.
     final Account account = deleteAccountsAndUnpickle(context, accountManager, TEST_FILENAME);
     assertNotNull(account);
+    assertEquals(TEST_USERNAME, account.name);
 
-    // Need to replace file.
+    // Now replace file with new username.
+    params = new SyncAccountParameters(context, accountManager,
+        TEST_USERNAME2, TEST_SYNCKEY, TEST_PASSWORD, TEST_SERVER_URL, null, TEST_CLIENT_NAME, TEST_CLIENT_GUID);
     AccountPickler.pickle(context, TEST_FILENAME, params, false);
 
-    try {
-      // Now try again -- should fail since account exists.
-      final Account account2 = AccountPickler.unpickle(context, TEST_FILENAME);
-      assertNull(account2);
-    } finally {
-      TestSyncAccounts.deleteAccount(this, accountManager, account);
+    // Checking if sync accounts exist could try to unpickle. That unpickle
+    // would load an account with a different username, so we can check that
+    // nothing was unpickled by verifying that the username has not changed.
+    assertTrue(SyncAccounts.syncAccountsExist(context));
+
+    for (Account a : getTestAccounts(accountManager)) {
+      assertEquals(TEST_USERNAME, a.name);
     }
   }
 }
