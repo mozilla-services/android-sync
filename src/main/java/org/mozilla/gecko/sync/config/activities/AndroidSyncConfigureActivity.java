e35dca0b8b5fc65a130e8d4ea5848473d9e9dc88
Chenxia Liu <liuche@mozilla.com>
2012-07-20 17:45:43 +0200

    Part 3: Manage engine state locally in SharedPrefs, and propagate changes to meta/global.

--- src/main/java/org/mozilla/gecko/sync/config/activities/AndroidSyncConfigureActivity.java
+++ src/main/java/org/mozilla/gecko/sync/config/activities/AndroidSyncConfigureActivity.java
@@ -3,7 +3,9 @@ package org.mozilla.gecko.sync.config.activities;
 
 import java.util.concurrent.TimeUnit;
 
+import org.mozilla.gecko.sync.GlobalConstants;
 import org.mozilla.gecko.sync.Logger;
+import org.mozilla.gecko.sync.SyncConfiguration;
 import org.mozilla.gecko.sync.ThreadPool;
 import org.mozilla.gecko.sync.Utils;
 import org.mozilla.gecko.sync.setup.Constants;
@@ -38,20 +40,19 @@ public abstract class AndroidSyncConfigureActivity extends Activity {
     mAccountManager = AccountManager.get(mContext);
   }
 
-  // hacky, because auth token storage is broken.
   private void invalidateAuthToken(Account account) {
     AccountManagerFuture<Bundle> future = mAccountManager.getAuthToken(account, Constants.AUTHTOKEN_TYPE_PLAIN, true, null, null);
     String token;
     try {
       token = future.getResult().getString(AccountManager.KEY_AUTHTOKEN);
-      mAccountManager.invalidateAuthToken(Constants.ACCOUNTTYPE_SYNC, token);
+      mAccountManager.invalidateAuthToken(GlobalConstants.ACCOUNTTYPE_SYNC, token);
     } catch (Exception e) {
       Logger.error(LOG_TAG, "Couldn't invalidate auth token: " + e);
     }
   }
 
   protected Account getAccount() {
-    Account[] accounts = mAccountManager.getAccountsByType(Constants.ACCOUNTTYPE_SYNC);
+    Account[] accounts = mAccountManager.getAccountsByType(GlobalConstants.ACCOUNTTYPE_SYNC);
     if (accounts.length == 0) {
       return null;
     } else {
@@ -88,8 +89,8 @@ public abstract class AndroidSyncConfigureActivity extends Activity {
               String serverURL = bundle.getString(Constants.OPTION_SERVER);
               Logger.debug(LOG_TAG, "username null? " + (null == username) + ", serverURL null? " + (null == serverURL));
               // Get SharedPreferences and display configuration editor.
-              String prefsPath;
-              prefsPath = Utils.getPrefsPath(username, serverURL);
+              // Prefs path args are taken directly from constants right now, because prefs are automatically migrated.
+              String prefsPath = Utils.getPrefsPath(GlobalConstants.BROWSER_INTENT_PACKAGE, username, serverURL, Constants.DEFAULT_PROFILE, SyncConfiguration.CURRENT_PREFS_VERSION);
               SharedPreferences prefs = mContext.getSharedPreferences(prefsPath, Utils.SHARED_PREFERENCES_MODE);
               accountConsumer.run(prefs);
             } catch (Exception e) {
--- src/main/java/org/mozilla/gecko/sync/config/activities/ConfigureEnginesActivity.java
+++ src/main/java/org/mozilla/gecko/sync/config/activities/ConfigureEnginesActivity.java
@@ -5,20 +5,21 @@
 package org.mozilla.gecko.sync.config.activities;
 
 import java.util.HashSet;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 
 import org.mozilla.gecko.R;
-import org.mozilla.gecko.db.BrowserContract;
 import org.mozilla.gecko.sync.Logger;
 import org.mozilla.gecko.sync.SyncConfiguration;
+import org.mozilla.gecko.sync.Utils;
+import org.mozilla.gecko.sync.setup.Constants;
 import org.mozilla.gecko.sync.setup.SyncAccounts.SyncAccountParameters;
 
-import android.accounts.Account;
 import android.app.AlertDialog;
-import android.content.ContentResolver;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
-import android.os.Bundle;
+import android.content.SharedPreferences.Editor;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -34,21 +35,9 @@ public class ConfigureEnginesActivity extends AndroidSyncConfigureActivity
     public void run(SyncAccountParameters syncParams, SharedPreferences prefs);
   }
 
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-  }
-
   protected String[]  _options = new String[] { "Bookmarks", "History", "Passwords", "Tabs" };
   protected boolean[] _selections = new boolean[_options.length];
-
-  // Hardcoded engines.
-  public void setSelections(Set<String> enabled) {
-    _selections[0] = enabled.contains("bookmarks");
-    _selections[1] = enabled.contains("forms") && enabled.contains("history");
-    _selections[2] = enabled.contains("passwords");
-    _selections[3] = enabled.contains("tabs");
-  }
+  private final boolean[] _origSelections = new boolean[_options.length];
 
   @Override
   public void onResume() {
@@ -60,7 +49,7 @@ public class ConfigureEnginesActivity extends AndroidSyncConfigureActivity
       @Override
       public void run(SharedPreferences prefs) {
         // Extract enabled-engine state.
-        Set<String> enabledEngineNames = SyncConfiguration.getEnabledEngineNames(prefs);
+        Set<String> enabledEngineNames = getEnabledEngines(prefs);
         if (enabledEngineNames == null) {
           enabledEngineNames = new HashSet<String>();
         }
@@ -69,39 +58,55 @@ public class ConfigureEnginesActivity extends AndroidSyncConfigureActivity
         new AlertDialog.Builder(self)
             .setTitle(R.string.sync_configure_engines_sync_my_title)
             .setMultiChoiceItems(_options, _selections, self)
-            .setIcon(R.drawable.sync_ic_launcher)
+            .setIcon(R.drawable.icon)
             .setPositiveButton(android.R.string.ok, self)
             .setNegativeButton(android.R.string.cancel, self).show();
       }
     });
   }
 
-  protected void requestSync() {
-    final Account account = getAccount();
-    if (account == null) {
-      Logger.error(LOG_TAG, "Failed to get account!");
-      return;
+  private Set<String> getEnabledEngines(SharedPreferences syncPrefs) {
+    Set<String> engines;
+    // Check engine SharedPrefs first.
+    SharedPreferences engineConfigPrefs = mContext.getSharedPreferences(Constants.PREFS_ENGINE_SELECTION, Utils.SHARED_PREFERENCES_MODE);
+    @SuppressWarnings("unchecked")
+    // We only add booleans to this SharedPreference.
+    Map<String, Boolean> engineMap = (Map<String, Boolean>) engineConfigPrefs.getAll();
+    if (!engineMap.isEmpty()) {
+      engines = new HashSet<String>();
+      for (Entry<String, Boolean> pair : engineMap.entrySet()) {
+        if (pair.getValue()) {
+          engines.add(pair.getKey());
+        }
+      }
+      return engines;
     }
+    Logger.warn(LOG_TAG, "No previous engine prefs");
+    // No previously stored engine prefs.
+    return SyncConfiguration.getEnabledEngineNames(syncPrefs);
+  }
 
-    final Bundle extras = new Bundle();
-    extras.putString("TEST", "test");
-    extras.putBoolean("force", true); // XXX not cool for a full sync??
+  // Hardcoded engines.
+  public void setSelections(Set<String> enabled) {
+    _selections[0] = enabled.contains("bookmarks");
+    // Omit Forms, because there is no way to enable/disable Forms in desktop UI.
+    _selections[1] = enabled.contains("history");
+    _selections[2] = enabled.contains("passwords");
+    _selections[3] = enabled.contains("tabs");
 
-    ContentResolver.requestSync(account, BrowserContract.AUTHORITY, extras);
+    // Cache original selections for comparing changes.
+    System.arraycopy(_selections, 0, _origSelections, 0, _selections.length);
   }
 
   @Override
   public void onClick(DialogInterface dialog, int which) {
-    if (which == DialogInterface.BUTTON_POSITIVE) {
-      requestSync();
+    if (which == DialogInterface.BUTTON_POSITIVE && saveEngines()) {
       setResult(RESULT_OK);
-      Toast.makeText(this, R.string.sync_notification_savedprefs, Toast.LENGTH_SHORT);
-      finish();
-    }
-    if (which == DialogInterface.BUTTON_NEGATIVE) {
+      Toast.makeText(this, R.string.sync_notification_savedprefs, Toast.LENGTH_SHORT).show();
+    } else {
       setResult(RESULT_CANCELED);
-      finish();
     }
+    finish();
   }
 
   @Override
@@ -112,5 +117,25 @@ public class ConfigureEnginesActivity extends AndroidSyncConfigureActivity
       selectionsList = ((AlertDialog) dialog).getListView();
     }
     selectionsList.setItemChecked(which, isChecked);
+  }
+
+  /**
+   * Persists enabled engines to SharedPreferences if changed.
+   * @return true if changed, false otherwise.
+   */
+  private boolean saveEngines() {
+    if (_selections.equals(_origSelections)) {
+      return false;
+    }
+    // Persist to SharedPreferences.
+    SharedPreferences prefs = mContext.getSharedPreferences(Constants.PREFS_ENGINE_SELECTION, Utils.SHARED_PREFERENCES_MODE);
+    Editor enginePrefsEditor = prefs.edit();
+    for (int i = 0; i < _selections.length; i++) {
+      if (_selections[i] != _origSelections[i]) {
+        enginePrefsEditor.putBoolean(_options[i].toLowerCase(), _selections[i]);
+      }
+    }
+    enginePrefsEditor.commit();
+    return true;
   }
 }
