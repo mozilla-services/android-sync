a413eb6354abec0bfd92a8388efaec8c37e5bb30
Nick Alexander <nalexander@mozilla.com>
2012-09-10 13:17:16 -0700

    Bug 787502 - post: Remove unused error handling from SyncStorageRecordRequest; it is still available in SyncStorageResponse.

--- src/main/java/org/mozilla/gecko/sync/net/SyncStorageRecordRequest.java
+++ src/main/java/org/mozilla/gecko/sync/net/SyncStorageRecordRequest.java
@@ -8,7 +8,6 @@ import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.security.GeneralSecurityException;
-import java.util.HashMap;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -45,48 +44,7 @@ import ch.boye.httpclientandroidlib.params.CoreProtocolPNames;
  * * Content-Type and Content-Length validation.
  */
 public class SyncStorageRecordRequest {
-  public static final String LOG_TAG = "SyncStorageRequest";
-
-  public static HashMap<String, String> SERVER_ERROR_MESSAGES;
-  static {
-    HashMap<String, String> errors = new HashMap<String, String>();
-
-    // Sync protocol errors.
-    errors.put("1", "Illegal method/protocol");
-    errors.put("2", "Incorrect/missing CAPTCHA");
-    errors.put("3", "Invalid/missing username");
-    errors.put("4", "Attempt to overwrite data that can't be overwritten (such as creating a user ID that already exists)");
-    errors.put("5", "User ID does not match account in path");
-    errors.put("6", "JSON parse failure");
-    errors.put("7", "Missing password field");
-    errors.put("8", "Invalid Weave Basic Object");
-    errors.put("9", "Requested password not strong enough");
-    errors.put("10", "Invalid/missing password reset code");
-    errors.put("11", "Unsupported function");
-    errors.put("12", "No email address on file");
-    errors.put("13", "Invalid collection");
-    errors.put("14", "User over quota");
-    errors.put("15", "The email does not match the username");
-    errors.put("16", "Client upgrade required");
-    errors.put("255", "An unexpected server error occurred: pool is empty.");
-
-    // Infrastructure-generated errors.
-    errors.put("\"server issue: getVS failed\"",                         "server issue: getVS failed");
-    errors.put("\"server issue: prefix not set\"",                       "server issue: prefix not set");
-    errors.put("\"server issue: host header not received from client\"", "server issue: host header not received from client");
-    errors.put("\"server issue: database lookup failed\"",               "server issue: database lookup failed");
-    errors.put("\"server issue: database is not healthy\"",              "server issue: database is not healthy");
-    errors.put("\"server issue: database not in pool\"",                 "server issue: database not in pool");
-    errors.put("\"server issue: database marked as down\"",              "server issue: database marked as down");
-    SERVER_ERROR_MESSAGES = errors;
-  }
-
-  public static String getServerErrorMessage(String body) {
-    if (SERVER_ERROR_MESSAGES.containsKey(body)) {
-      return SERVER_ERROR_MESSAGES.get(body);
-    }
-    return body;
-  }
+  public static final String LOG_TAG = "SyncStorageRecordRequest";
 
   protected static StringEntity stringEntity(String s)
       throws UnsupportedEncodingException {
