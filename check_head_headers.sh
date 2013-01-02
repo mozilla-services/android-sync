df895d03f39ad6049d3459871263749c35ca1b4d
Nick Alexander <nalexander@mozilla.com>
2013-01-02 14:41:54 -0800

    No bug - make check_head_headers.sh suitable for a git pre-commit hook.

--- check_head_headers.sh
+++ check_head_headers.sh
@@ -2,17 +2,21 @@
 
 # Verify that HEAD headers are up to date.
 
+exitcode=0
+
 # first files commited but not merged to develop.
-python "tools/check_headers.py" -l "package" -f "tools/COPYRIGHT_MPL" -x "test" "$@" `git diff --name-only develop HEAD | grep ^src/main/java/org/mozilla/`
-python "tools/check_headers.py" -l "package" -f "tools/COPYRIGHT_PD" "$@"            `git diff --name-only develop HEAD | grep ^src/test/java/org/mozilla`
-python "tools/check_headers.py" -l "package" -f "tools/COPYRIGHT_PD" "$@"            `git diff --name-only develop HEAD | grep ^test/src/org/mozilla`
+python "tools/check_headers.py" -l "package" -f "tools/COPYRIGHT_MPL" -x "test" "$@" `git diff --name-only develop HEAD | grep ^src/main/java/org/mozilla/` || exitcode=$?
+python "tools/check_headers.py" -l "package" -f "tools/COPYRIGHT_PD" "$@"            `git diff --name-only develop HEAD | grep ^src/test/java/org/mozilla` || exitcode=$?
+python "tools/check_headers.py" -l "package" -f "tools/COPYRIGHT_PD" "$@"            `git diff --name-only develop HEAD | grep ^test/src/org/mozilla` || exitcode=$?
 
 # second files we are modifying.
-python "tools/check_headers.py" -l "package" -f "tools/COPYRIGHT_MPL" -x "test" "$@" `git ls-files --modified | grep ^src/main/java/org/mozilla/`
-python "tools/check_headers.py" -l "package" -f "tools/COPYRIGHT_PD" "$@"            `git ls-files --modified | grep ^src/test/java/org/mozilla`
-python "tools/check_headers.py" -l "package" -f "tools/COPYRIGHT_PD" "$@"            `git ls-files --modified | grep ^test/src/org/mozilla`
+python "tools/check_headers.py" -l "package" -f "tools/COPYRIGHT_MPL" -x "test" "$@" `git ls-files --modified | grep ^src/main/java/org/mozilla/` || exitcode=$?
+python "tools/check_headers.py" -l "package" -f "tools/COPYRIGHT_PD" "$@"            `git ls-files --modified | grep ^src/test/java/org/mozilla` || exitcode=$?
+python "tools/check_headers.py" -l "package" -f "tools/COPYRIGHT_PD" "$@"            `git ls-files --modified | grep ^test/src/org/mozilla` || exitcode=$?
 
 # third files that are new and are staged.
-python "tools/check_headers.py" -l "package" -f "tools/COPYRIGHT_MPL" -x "test" "$@" `git diff --name-only --cached | grep ^src/main/java/org/mozilla/`
-python "tools/check_headers.py" -l "package" -f "tools/COPYRIGHT_PD" "$@"            `git diff --name-only --cached | grep ^src/test/java/org/mozilla`
-python "tools/check_headers.py" -l "package" -f "tools/COPYRIGHT_PD" "$@"            `git diff --name-only --cached | grep ^test/src/org/mozilla`
+python "tools/check_headers.py" -l "package" -f "tools/COPYRIGHT_MPL" -x "test" "$@" `git diff --name-only --cached | grep ^src/main/java/org/mozilla/` || exitcode=$?
+python "tools/check_headers.py" -l "package" -f "tools/COPYRIGHT_PD" "$@"            `git diff --name-only --cached | grep ^src/test/java/org/mozilla` || exitcode=$?
+python "tools/check_headers.py" -l "package" -f "tools/COPYRIGHT_PD" "$@"            `git diff --name-only --cached | grep ^test/src/org/mozilla` || exitcode=$?
+
+exit $exitcode
