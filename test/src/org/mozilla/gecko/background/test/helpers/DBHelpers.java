4eaab8c0a73056f093acebe7732f22e6d31665c2
Richard Newman <rnewman@twinql.com>
2013-05-16 17:07:06 -0700

    Bug 858742 - Part 0: Add DBHelpers, FakeProfileTestCase, DBProviderTestCase.

---
+++ test/src/org/mozilla/gecko/background/test/helpers/DBHelpers.java
@@ -0,0 +1,36 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.gecko.background.test.helpers;
+
+import android.database.Cursor;
+import junit.framework.Assert;
+
+public class DBHelpers {
+
+  /*
+   * Works for strings and int-ish values.
+   */
+  public static void assertCursorContains(Object[][] expected, Cursor actual) {
+    Assert.assertEquals(expected.length, actual.getCount());
+    int i = 0, j = 0;
+    Object[] row;
+
+    do {
+      row = expected[i];
+      for (j = 0; j < row.length; ++j) {
+        Object atIndex = row[j];
+        if (atIndex == null) {
+          continue;
+        }
+        if (atIndex instanceof String) {
+          Assert.assertEquals(atIndex, actual.getString(j));
+        } else {
+          Assert.assertEquals(atIndex, actual.getInt(j));
+        }
+      }
+      ++i;
+    } while (actual.moveToPosition(i));
+  }
+}
