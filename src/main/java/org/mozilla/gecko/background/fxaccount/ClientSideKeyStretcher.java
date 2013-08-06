9326a50dc741e8fb484d5bd1601c187d4a91e360
Nick Alexander <nalexander@mozilla.com>
2013-08-06 14:14:01 -0700

    Bug STRETCH: first version of PICL key stretching.
    This version is built on PBKDF2 and scrypt.  The algorithm is
    described at
    https://wiki.mozilla.org/Identity/AttachedServices/KeyServerProtocol#Client-Side_Key_Stretching.

---
+++ src/main/java/org/mozilla/gecko/background/fxaccount/ClientSideKeyStretcher.java
@@ -0,0 +1,24 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.gecko.background.fxaccount;
+
+import java.io.UnsupportedEncodingException;
+import java.security.GeneralSecurityException;
+
+import org.json.simple.JSONObject;
+
+public interface ClientSideKeyStretcher {
+  /**
+   * Stretch a user entered password.
+   *
+   * @param email associated with account.
+   * @param password to stretch.
+   * @param options algorithm specific options.
+   * @return stretched password.
+   * @throws UnsupportedEncodingException when the email or password cannot be converted to UTF-8 bytes (should never happen).
+   * @throws GeneralSecurityException when the hashing or cryptography fails entirely.
+   */
+  public byte[] stretch(String email, String password, JSONObject options) throws UnsupportedEncodingException, GeneralSecurityException;
+}
---
+++ src/main/java/org/mozilla/gecko/background/fxaccount/PICLClientSideKeyStretcher.java
@@ -0,0 +1,44 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.gecko.background.fxaccount;
+
+import java.io.UnsupportedEncodingException;
+import java.security.GeneralSecurityException;
+
+import org.json.simple.JSONObject;
+import org.mozilla.gecko.sync.Utils;
+import org.mozilla.gecko.sync.crypto.PBKDF2;
+
+import com.lambdaworks.crypto.SCrypt;
+
+public class PICLClientSideKeyStretcher implements ClientSideKeyStretcher {
+  /**
+   * Stretch a user entered password, following
+   * <a href="https://wiki.mozilla.org/Identity/AttachedServices/KeyServerProtocol#Client-Side_Key_Stretching">https://wiki.mozilla.org/Identity/AttachedServices/KeyServerProtocol#Client-Side_Key_Stretching</a>.
+   */
+  public byte[] stretch(String email, String password, JSONObject options) throws UnsupportedEncodingException, GeneralSecurityException {
+    if (email == null) {
+      throw new IllegalArgumentException("email must not be null");
+    }
+    if (password == null) {
+      throw new IllegalArgumentException("password must not be null.");
+    }
+
+    if (options != null) {
+      throw new IllegalArgumentException("No options accepted.");
+    }
+
+    byte[] emailUTF8 = email.getBytes("UTF-8");
+    byte[] passwordUTF8 = password.getBytes("UTF-8");
+
+    byte[] k1 = PBKDF2.pbkdf2SHA256(passwordUTF8, FxAccountUtils.KWE("first-PBKDF", emailUTF8), 20*1000, 1*32);
+    byte[] k2 = SCrypt.scrypt(k1, FxAccountUtils.KW("scrypt"), 64*1024, 8, 1, 1*32);
+
+    byte[] in = Utils.concatAll(k2, passwordUTF8);
+    byte[] stretchedPW = PBKDF2.pbkdf2SHA256(in, FxAccountUtils.KWE("second-PBKDF", emailUTF8), 20*1000, 1*32);
+
+    return stretchedPW;
+  }
+}
