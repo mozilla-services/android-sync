87669e3fd5cee9b562ef9181302bad7bbaa96304
Michael Comella <michael.l.comella@gmail.com>
2013-08-16 13:56:58 -0700

    Bug 870171 - Part 12: Add HealthReportPruneService skeleton.

---
+++ src/main/java/org/mozilla/gecko/background/healthreport/prune/HealthReportPruneService.java
@@ -0,0 +1,38 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.gecko.background.healthreport.prune;
+
+import org.mozilla.gecko.background.BackgroundService;
+import org.mozilla.gecko.background.common.log.Logger;
+import org.mozilla.gecko.background.healthreport.HealthReportConstants;
+
+import android.content.Intent;
+import android.os.IBinder;
+
+/**
+ * A <code>Service</code> to prune unnecessary or excessive health report data.
+ *
+ * We extend <code>IntentService</code>, rather than just <code>Service</code>,
+ * because this gives us a worker thread to avoid excessive main-thread disk access.
+ */
+public class HealthReportPruneService extends BackgroundService {
+  public static final String LOG_TAG = HealthReportPruneService.class.getSimpleName();
+  public static final String WORKER_THREAD_NAME = LOG_TAG + "Worker";
+
+  public HealthReportPruneService() {
+    super(WORKER_THREAD_NAME);
+  }
+
+  @Override
+  public IBinder onBind(Intent intent) {
+    return null;
+  }
+
+  @Override
+  public void onHandleIntent(Intent intent) {
+    Logger.setThreadLogTag(HealthReportConstants.GLOBAL_LOG_TAG);
+    Logger.debug(LOG_TAG, "Handling prune intent.");
+  }
+}
