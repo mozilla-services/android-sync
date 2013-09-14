d95403c7a4cf712c9aa91c31ad3cf6f0acd63a1f
Richard Newman <rnewman@twinql.com>
2013-09-13 17:51:21 -0700

    Bug 916316 - Part 3: reimplement date formatting, allow memoization of day formatting. Benchmark tests.

--- src/main/java/org/mozilla/gecko/background/common/DateUtils.java
+++ src/main/java/org/mozilla/gecko/background/common/DateUtils.java
@@ -4,22 +4,44 @@
 
 package org.mozilla.gecko.background.common;
 
-import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Locale;
 import java.util.TimeZone;
 
+import android.util.SparseArray;
+
 public class DateUtils {
+  private static final TimeZone UTC = TimeZone.getTimeZone("UTC");
+
   public static String getDateString(long time) {
-    final SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd", Locale.US);
-    format.setTimeZone(TimeZone.getTimeZone("UTC"));
-    return format.format(time);  
+    Calendar d = Calendar.getInstance();
+    d.setTimeZone(UTC);
+    d.setTimeInMillis(time);
+    return String.format(Locale.US,
+                         "%04d-%02d-%02d",
+                         d.get(Calendar.YEAR),
+                         d.get(Calendar.MONTH) + 1,      // 0-indexed.
+                         d.get(Calendar.DAY_OF_MONTH));
+  }
+
+  public static String getDateStringForDay(long day) {
+    return getDateString(GlobalConstants.MILLISECONDS_PER_DAY * day);
+  }
+
+  public static String getDateStringForDay(long day, SparseArray<String> memo) {
+    // Truncating is fine -- it's a day, not a timestamp -- and necessary
+    // to use SparseArray.
+    int dd = (int) day;
+    String s = memo.get(dd);
+    if (s == null) {
+      s = getDateString(GlobalConstants.MILLISECONDS_PER_DAY * day);
+      memo.put(dd, s);
+    }
+    return s;
   }
 
   public static int getDay(final long time) {
     return (int) Math.floor(time / GlobalConstants.MILLISECONDS_PER_DAY);
   }
 
-  public static String getDateStringForDay(long day) {
-    return DateUtils.getDateString(GlobalConstants.MILLISECONDS_PER_DAY * day);
-  }
 }
