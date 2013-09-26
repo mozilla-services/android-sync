3e2dba9aeffbff0cb0d18366e325bde0022a0f22
Michael Comella <michael.l.comella@gmail.com>
2013-09-26 16:55:03 -0700

    Bug 870925 - Part 12: Add success and null document tests.

--- test/src/org/mozilla/gecko/background/healthreport/upload/TestAndroidSubmissionClient.java
+++ test/src/org/mozilla/gecko/background/healthreport/upload/TestAndroidSubmissionClient.java
@@ -6,6 +6,7 @@ package org.mozilla.gecko.background.healthreport.upload;
 import java.util.Collection;
 
 import org.mozilla.gecko.background.bagheera.BagheeraRequestDelegate;
+import org.mozilla.gecko.background.healthreport.HealthReportStorage;
 import org.mozilla.gecko.background.healthreport.HealthReportDatabaseStorage;
 import org.mozilla.gecko.background.healthreport.MockHealthReportDatabaseStorage.PrepopulatedMockHealthReportDatabaseStorage;
 import org.mozilla.gecko.background.healthreport.MockProfileInformationCache;
@@ -18,11 +19,14 @@ import org.mozilla.gecko.background.helpers.FakeProfileTestCase;
 import android.content.ContentProviderClient;
 import android.content.Context;
 import android.content.SharedPreferences;
+import org.json.JSONObject;
 
 public class TestAndroidSubmissionClient extends FakeProfileTestCase {
   public static class MockAndroidSubmissionClient extends AndroidSubmissionClient {
     protected final PrepopulatedMockHealthReportDatabaseStorage storage;
 
+    public SubmissionState submissionState = SubmissionState.SUCCESS;
+    public DocumentStatus documentStatus = DocumentStatus.VALID;
     public boolean hasUploadBeenRequested = true;
 
     public MockAndroidSubmissionClient(final Context context, final SharedPreferences sharedPrefs,
@@ -45,6 +49,24 @@ public class TestAndroidSubmissionClient extends FakeProfileTestCase {
     }
 
     @Override
+    public JSONObject generateDocument(final HealthReportStorage storage, final long localTime,
+        final long last, final String profilePath) {
+      switch (documentStatus) {
+      case VALID:
+        return new JSONObject(); // Beyond == null, we don't check for valid FHR documents.
+
+      case NULL:
+        return null;
+
+      case EXCEPTION:
+        throw new IllegalStateException("Intended Exception");
+
+      default:
+        throw new IllegalStateException("Unintended Exception");
+      }
+    }
+
+    @Override
     public boolean hasUploadBeenRequested() {
       return hasUploadBeenRequested;
     }
@@ -52,9 +74,34 @@ public class TestAndroidSubmissionClient extends FakeProfileTestCase {
     @Override
     protected void uploadPayload(String id, String payload, Collection<String> oldIds,
         BagheeraRequestDelegate delegate) {
-      // Do nothing so we don't connect to the network.
+      switch (submissionState) {
+      case SUCCESS:
+        delegate.handleSuccess(0, null, id, null);
+        break;
+
+      case FAILURE:
+        delegate.handleFailure(0, null, null);
+        break;
+
+      case ERROR:
+        delegate.handleError(null);
+        break;
+
+      default:
+        throw new IllegalStateException("Unknown submissionState, " + submissionState);
+      }
     }
   }
+
+  public final SubmissionsFieldName[] SUBMISSIONS_STATUS_FIELD_NAMES = new SubmissionsFieldName[] {
+    SubmissionsFieldName.SUCCESS,
+    SubmissionsFieldName.CLIENT_FAILURE,
+    SubmissionsFieldName.TRANSPORT_FAILURE,
+    SubmissionsFieldName.SERVER_FAILURE
+  };
+
+  public static enum SubmissionState { SUCCESS, FAILURE, ERROR }
+  public static enum DocumentStatus { VALID, NULL, EXCEPTION };
 
   public StubDelegate stubDelegate;
   public PrepopulatedMockHealthReportDatabaseStorage storage;
@@ -83,5 +130,36 @@ public class TestAndroidSubmissionClient extends FakeProfileTestCase {
     client.upload(storage.now, null, null, stubDelegate);
     assertEquals(0, getSubmissionsCount(SubmissionsFieldName.FIRST_ATTEMPT));
     assertEquals(1, getSubmissionsCount(SubmissionsFieldName.CONTINUATION_ATTEMPT));
+  }
+
+  /**
+   * Asserts that the given field name has a count of 1 and all other status (success and failures)
+   * have a count of 0.
+   */
+  public void assertStatusCount(final SubmissionsFieldName fieldName) {
+    for (SubmissionsFieldName name : SUBMISSIONS_STATUS_FIELD_NAMES) {
+      if (name == fieldName) {
+        assertEquals(1, getSubmissionsCount(name));
+      } else {
+        assertEquals(0, getSubmissionsCount(name));
+      }
+    }
+  }
+
+  public void testUploadSubmissionsSuccessCount() throws Exception {
+    client.upload(storage.now, null, null, stubDelegate);
+    assertStatusCount(SubmissionsFieldName.SUCCESS);
+  }
+
+  public void testUploadNullDocumentSubmissionsFailureCount() throws Exception {
+    client.documentStatus = DocumentStatus.NULL;
+    client.upload(storage.now, null, null, stubDelegate);
+    assertStatusCount(SubmissionsFieldName.CLIENT_FAILURE);
+  }
+
+  public void testUploadDocumentGenerationExceptionSubmissionsFailureCount() throws Exception {
+    client.documentStatus = DocumentStatus.EXCEPTION;
+    client.upload(storage.now, null, null, stubDelegate);
+    assertStatusCount(SubmissionsFieldName.CLIENT_FAILURE);
   }
 }
