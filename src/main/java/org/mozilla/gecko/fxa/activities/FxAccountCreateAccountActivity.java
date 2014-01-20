39f4fa2965edb9a22f1458d0734cf6d0116bfad9
Nick Alexander <nalexander@mozilla.com>
2014-01-20 15:16:34 -0800

    Bug 961184 - Part 2: Rework activities shown after CreateAccount and SignIn.

--- src/main/java/org/mozilla/gecko/fxa/activities/FxAccountCreateAccountActivity.java
+++ src/main/java/org/mozilla/gecko/fxa/activities/FxAccountCreateAccountActivity.java
@@ -182,6 +182,14 @@ public class FxAccountCreateAccountActivity extends FxAccountAbstractSetupActivi
       // intent.putExtra(AccountManager.KEY_AUTHTOKEN, accountType);
       setResult(RESULT_OK, intent);
       finish();
+
+      // Show success activity.
+      Intent successIntent = new Intent(FxAccountCreateAccountActivity.this, FxAccountConfirmAccountActivity.class);
+      successIntent.putExtra("email", email);
+      // Per http://stackoverflow.com/a/8992365, this triggers a known bug with
+      // the soft keyboard not being shown for the started activity. Why, Android, why?
+      successIntent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+      startActivity(successIntent);
     }
   }
 
--- src/main/java/org/mozilla/gecko/fxa/activities/FxAccountSignInActivity.java
+++ src/main/java/org/mozilla/gecko/fxa/activities/FxAccountSignInActivity.java
@@ -151,6 +151,15 @@ public class FxAccountSignInActivity extends FxAccountAbstractSetupActivity {
       // intent.putExtra(AccountManager.KEY_AUTHTOKEN, accountType);
       setResult(RESULT_OK, intent);
       finish();
+
+      // Show success activity depending on verification status.
+      Intent successIntent = new Intent(FxAccountSignInActivity.this,
+          result.verified ? FxAccountVerifiedAccountActivity.class : FxAccountConfirmAccountActivity.class);
+      successIntent.putExtra("email", email);
+      // Per http://stackoverflow.com/a/8992365, this triggers a known bug with
+      // the soft keyboard not being shown for the started activity. Why, Android, why?
+      successIntent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+      startActivity(successIntent);
     }
   }
 
