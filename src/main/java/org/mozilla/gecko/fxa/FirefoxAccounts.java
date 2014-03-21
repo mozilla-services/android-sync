7f671664bde2441433bd835cd1e7b078c6fd8062
Michael Comella <michael.l.comella@gmail.com>
2014-03-21 14:40:05 -0700

    Bug 957894 - Review comment: Move unpickle call and async stuff to getFirefoxAccounts.

--- src/main/java/org/mozilla/gecko/fxa/FirefoxAccounts.java
+++ src/main/java/org/mozilla/gecko/fxa/FirefoxAccounts.java
@@ -5,9 +5,11 @@
 package org.mozilla.gecko.fxa;
 
 import java.io.File;
+import java.util.concurrent.CountDownLatch;
 
 import org.mozilla.gecko.fxa.authenticator.AccountPickler;
 import org.mozilla.gecko.fxa.authenticator.AndroidFxAccount;
+import org.mozilla.gecko.sync.ThreadPool;
 
 import android.accounts.Account;
 import android.accounts.AccountManager;
@@ -18,41 +20,84 @@ import android.content.Context;
  */
 public class FirefoxAccounts {
   /**
-   * Return true if at least one Firefox account exists. If no accounts exist in the
-   * AccountManager, one may be created via a pickled Firefox account, if available, and true
-   * will be returned.
-   * <p>
-   * Do not call this method from the main thread.
+   * Container used to encapsulate an {@link android.accounts.Account Account}. Used to
+   * return a value from an asynchronous operation. Note that this container is not
+   * synchronized.
+   */
+  private static class AccountContainer {
+    private Account account;
+
+    public AccountContainer() {
+      account = null;
+    }
+
+    public Account getAccount() { return account; }
+    public void setAccount(final Account account) { this.account = account; }
+  }
+
+  /**
+   * Returns true if a FirefoxAccount exists, false otherwise.
    *
    * @param context Android context.
    * @return true if at least one Firefox account exists.
    */
   public static boolean firefoxAccountsExist(final Context context) {
-    if (getFirefoxAccounts(context).length > 0) {
-      return true;
-    }
-
-    final File file = context.getFileStreamPath(FxAccountConstants.ACCOUNT_PICKLE_FILENAME);
-    if (!file.exists()) {
-      return false;
-    }
-
-    // There is a small race window here: if the user creates a new Firefox account
-    // between our checks, this could erroneously report that no Firefox accounts
-    // exist.
-    final AndroidFxAccount account =
-        AccountPickler.unpickle(context, FxAccountConstants.ACCOUNT_PICKLE_FILENAME);
-    return (account != null);
+    return getFirefoxAccounts(context).length > 0;
   }
 
   /**
-   * Return Firefox accounts.
+   * Return Firefox accounts. If no accounts exist in the AccountManager,
+   * one may be created via a pickled FirefoxAccount, if available, and that
+   * account will be added to the AccountManager and returned.
    *
    * @param context Android context.
    * @return Firefox account objects.
    */
   public static Account[] getFirefoxAccounts(final Context context) {
-    return AccountManager.get(context).getAccountsByType(FxAccountConstants.ACCOUNT_TYPE);
+    final Account[] accounts =
+        AccountManager.get(context).getAccountsByType(FxAccountConstants.ACCOUNT_TYPE);
+    if (accounts.length > 0) {
+      return accounts;
+    }
+
+    final Account pickledAccount = getPickledAccount(context);
+    return (pickledAccount != null) ? new Account[] {pickledAccount} : new Account[0];
+  }
+
+  private static Account getPickledAccount(final Context context) {
+    // To avoid a StrictMode violation for disk access, we call this from a background thread.
+    // We do this every time, so the caller doesn't have to care.
+    final CountDownLatch latch = new CountDownLatch(1);
+    final AccountContainer accountContainer = new AccountContainer();
+    ThreadPool.run(new Runnable() {
+      @Override
+      public void run() {
+        try {
+          final File file = context.getFileStreamPath(FxAccountConstants.ACCOUNT_PICKLE_FILENAME);
+          if (!file.exists()) {
+            accountContainer.setAccount(null);
+            return;
+          }
+
+          // There is a small race window here: if the user creates a new Firefox account
+          // between our checks, this could erroneously report that no Firefox accounts
+          // exist.
+          final AndroidFxAccount fxAccount =
+              AccountPickler.unpickle(context, FxAccountConstants.ACCOUNT_PICKLE_FILENAME);
+          accountContainer.setAccount(fxAccount.getAndroidAccount());
+        } finally {
+          latch.countDown();
+        }
+      }
+    });
+
+    try {
+      latch.await(); // the background thread.
+    } catch (InterruptedException e) {
+      throw new IllegalStateException("Thread unexpectedly interrupted", e);
+    }
+
+    return accountContainer.getAccount();
   }
 
   /**
