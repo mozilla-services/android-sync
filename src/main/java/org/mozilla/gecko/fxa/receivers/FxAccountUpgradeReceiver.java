ddabe006fb3e5b0aed9be42fe691c21dd5a69ddc
Nick Alexander <nalexander@mozilla.com>
2014-03-27 11:36:36 -0700

    Bug 988571 - Part 3: Move accounts from the Doghouse to Separated on upgrade.

--- src/main/java/org/mozilla/gecko/fxa/receivers/FxAccountUpgradeReceiver.java
+++ src/main/java/org/mozilla/gecko/fxa/receivers/FxAccountUpgradeReceiver.java
@@ -12,7 +12,12 @@ import java.util.concurrent.Executors;
 import org.mozilla.gecko.background.common.log.Logger;
 import org.mozilla.gecko.fxa.FirefoxAccounts;
 import org.mozilla.gecko.fxa.FxAccountConstants;
+import org.mozilla.gecko.fxa.authenticator.AndroidFxAccount;
+import org.mozilla.gecko.fxa.login.State;
+import org.mozilla.gecko.fxa.login.State.StateLabel;
+import org.mozilla.gecko.sync.Utils;
 
+import android.accounts.Account;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -36,6 +41,9 @@ public class FxAccountUpgradeReceiver extends BroadcastReceiver {
   protected List<Runnable> onUpgradeRunnables(Context context) {
     List<Runnable> runnables = new LinkedList<Runnable>();
     runnables.add(new MaybeUnpickleRunnable(context));
+    // Recovering accounts that are in the Doghouse should happen *after* we
+    // unpickle any accounts saved to disk.
+    runnables.add(new AdvanceFromDoghouseRunnable(context));
     return runnables;
   }
 
@@ -77,6 +85,48 @@ public class FxAccountUpgradeReceiver extends BroadcastReceiver {
       // Querying the accounts will unpickle any pickled Firefox Account.
       Logger.info(LOG_TAG, "Trying to unpickle any pickled Firefox Account.");
       FirefoxAccounts.getFirefoxAccounts(context);
+    }
+  }
+
+  /**
+   * A Runnable that tries to advance existing Firefox Accounts that are in the
+   * Doghouse state to the Separated state.
+   * <p>
+   * This is our main deprecation-and-upgrade mechanism: in some way, the
+   * Account gets moved to the Doghouse state. If possible, an upgraded version
+   * of the package advances to Separated, prompting the user to re-connect the
+   * Account.
+   */
+  protected static class AdvanceFromDoghouseRunnable implements Runnable {
+    protected final Context context;
+
+    public AdvanceFromDoghouseRunnable(Context context) {
+      this.context = context;
+    }
+
+    @Override
+    public void run() {
+      final Account[] accounts = FirefoxAccounts.getFirefoxAccounts(context);
+      Logger.info(LOG_TAG, "Trying to advance " + accounts.length + " existing Firefox Accounts from the Doghouse to Separated (if necessary).");
+      for (Account account : accounts) {
+        try {
+          final AndroidFxAccount fxAccount = new AndroidFxAccount(context, account);
+          // For great debugging.
+          if (FxAccountConstants.LOG_PERSONAL_INFORMATION) {
+            fxAccount.dump();
+          }
+          State state = fxAccount.getState();
+          if (state == null || state.getStateLabel() != StateLabel.Doghouse) {
+            Logger.debug(LOG_TAG, "Account named like " + Utils.obfuscateEmail(account.name) + " is not in the Doghouse; skipping.");
+            continue;
+          }
+          Logger.debug(LOG_TAG, "Account named like " + Utils.obfuscateEmail(account.name) + " is in the Doghouse; advancing to Separated.");
+          fxAccount.setState(state.makeSeparatedState());
+        } catch (Exception e) {
+          Logger.warn(LOG_TAG, "Got exception trying to advance account named like " + Utils.obfuscateEmail(account.name) +
+              " from Doghouse to Separated state; ignoring.", e);
+        }
+      }
     }
   }
 }
