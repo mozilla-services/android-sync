5956dfaee1153dbd0073ebbe1366e4122b79870f
Nick Alexander <nalexander@mozilla.com>
2014-06-03 17:02:15 -0700

    Bug 993134 - Part 4: Make fxa.TestAccountPickler robust.
    This tested extracting data from an AndroidFxAccount before, and another
    after, backed by different Android Accounts.  Unfortunately, one Account
    replaced the other, but had the same name, leading to a race
    condition (between reading and deleting the first account).  To address
    this, we extract all the data before deleting, and then compare it to
    the data from the newly pickled account.

--- test/src/org/mozilla/gecko/background/fxa/authenticator/TestAccountPickler.java
+++ test/src/org/mozilla/gecko/background/fxa/authenticator/TestAccountPickler.java
@@ -9,6 +9,7 @@ import org.mozilla.gecko.fxa.authenticator.AccountPickler;
 import org.mozilla.gecko.fxa.authenticator.AndroidFxAccount;
 import org.mozilla.gecko.fxa.login.Separated;
 import org.mozilla.gecko.fxa.login.State;
+import org.mozilla.gecko.sync.ExtendedJSONObject;
 import org.mozilla.gecko.sync.Utils;
 
 import android.accounts.Account;
@@ -16,6 +17,9 @@ import android.accounts.AccountManager;
 import android.test.RenamingDelegatingContext;
 
 public class TestAccountPickler extends AndroidSyncTestCaseWithAccounts {
+  private static final String TEST_TOKEN_SERVER_URI = "tokenServerURI";
+  private static final String TEST_AUTH_SERVER_URI = "serverURI";
+  private static final String TEST_PROFILE = "profile";
   private final static String FILENAME_PREFIX = "TestAccountPickler-";
   private final static String PICKLE_FILENAME = "pickle";
 
@@ -51,12 +55,35 @@ public class TestAccountPickler extends AndroidSyncTestCaseWithAccounts {
   public AndroidFxAccount addTestAccount() throws Exception {
     final State state = new Separated(TEST_USERNAME, "uid", false); // State choice is arbitrary.
     final AndroidFxAccount account = AndroidFxAccount.addAndroidAccount(context, TEST_USERNAME,
-        "profile", "serverURI", "tokenServerURI", state);
+        TEST_PROFILE, TEST_AUTH_SERVER_URI, TEST_TOKEN_SERVER_URI, state);
     assertNotNull(account);
     assertNotNull(account.getProfile());
     assertTrue(testAccountsExist()); // Sanity check.
     this.account = account.getAndroidAccount(); // To remove in tearDown() if we throw.
     return account;
+  }
+
+  public void testPickle() throws Exception {
+    final AndroidFxAccount account = addTestAccount();
+    // Sync is enabled by default so we do a more thorough test by disabling it.
+    account.disableSyncing();
+
+    final long now = System.currentTimeMillis();
+    final ExtendedJSONObject o = AccountPickler.toJSON(account, now);
+
+    assertEquals(2, o.getLong(AccountPickler.KEY_PICKLE_VERSION).longValue());
+    assertTrue(o.getLong(AccountPickler.KEY_PICKLE_TIMESTAMP).longValue() < System.currentTimeMillis());
+
+    assertEquals(AndroidFxAccount.CURRENT_ACCOUNT_VERSION, o.getIntegerSafely(AccountPickler.KEY_ACCOUNT_VERSION).intValue());
+    assertEquals(FxAccountConstants.ACCOUNT_TYPE, o.getString(AccountPickler.KEY_ACCOUNT_TYPE));
+
+    assertEquals(TEST_USERNAME, o.getString(AccountPickler.KEY_EMAIL));
+    assertEquals(TEST_PROFILE, o.getString(AccountPickler.KEY_PROFILE));
+    assertEquals(TEST_AUTH_SERVER_URI, o.getString(AccountPickler.KEY_IDP_SERVER_URI));
+    assertEquals(TEST_TOKEN_SERVER_URI, o.getString(AccountPickler.KEY_TOKEN_SERVER_URI));
+
+    assertFalse(o.getBoolean(AccountPickler.KEY_IS_SYNCING_ENABLED));
+    assertNotNull(o.get(AccountPickler.KEY_BUNDLE));
   }
 
   public void testPickleAndUnpickle() throws Exception {
@@ -65,16 +92,24 @@ public class TestAccountPickler extends AndroidSyncTestCaseWithAccounts {
     inputAccount.disableSyncing();
 
     AccountPickler.pickle(inputAccount, PICKLE_FILENAME);
+    final ExtendedJSONObject inputJSON = AccountPickler.toJSON(inputAccount, 0);
+    final State inputState = inputAccount.getState();
+    assertNotNull(inputJSON);
+    assertNotNull(inputState);
 
     // unpickle adds an account to the AccountManager so delete it first.
     deleteTestAccounts();
     assertFalse(testAccountsExist());
 
-    final AndroidFxAccount unpickledAccount =
-        AccountPickler.unpickle(context, PICKLE_FILENAME);
+    final AndroidFxAccount unpickledAccount = AccountPickler.unpickle(context, PICKLE_FILENAME);
     assertNotNull(unpickledAccount);
-    this.account = unpickledAccount.getAndroidAccount(); // To remove in tearDown().
-    assertAccountsEquals(inputAccount, unpickledAccount);
+    final ExtendedJSONObject unpickledJSON = AccountPickler.toJSON(unpickledAccount, 0);
+    final State unpickledState = unpickledAccount.getState();
+    assertNotNull(unpickledJSON);
+    assertNotNull(unpickledState);
+
+    assertEquals(inputJSON, unpickledJSON);
+    assertStateEquals(inputState, unpickledState);
   }
 
   public void testDeletePickle() throws Exception {
@@ -87,21 +122,6 @@ public class TestAccountPickler extends AndroidSyncTestCaseWithAccounts {
 
     AccountPickler.deletePickle(context, PICKLE_FILENAME);
     assertFileNotPresent(context, PICKLE_FILENAME);
-  }
-
-  private void assertAccountsEquals(final AndroidFxAccount expected,
-      final AndroidFxAccount actual) throws Exception {
-    // TODO: Write and use AndroidFxAccount.equals
-    // TODO: protected.
-    //assertEquals(expected.getAccountVersion(), actual.getAccountVersion());
-    assertEquals(expected.getProfile(), actual.getProfile());
-    assertEquals(expected.getAccountServerURI(), actual.getAccountServerURI());
-    assertEquals(expected.getAudience(), actual.getAudience());
-    assertEquals(expected.getTokenServerURI(), actual.getTokenServerURI());
-    assertEquals(expected.getSyncPrefsPath(), actual.getSyncPrefsPath());
-    assertEquals(expected.isSyncing(), actual.isSyncing());
-    assertEquals(expected.getEmail(), actual.getEmail());
-    assertStateEquals(expected.getState(), actual.getState());
   }
 
   private void assertStateEquals(final State expected, final State actual) throws Exception {
