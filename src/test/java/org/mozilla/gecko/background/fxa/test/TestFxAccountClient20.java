b4029585aac5cc07f17027b804295258f8390c78
Nick Alexander <nalexander@mozilla.com>
2015-01-12 12:46:22 -0800

    Bug 1119070 - Part 1: Extract uniform getBaseResource.

--- src/test/java/org/mozilla/gecko/background/fxa/test/TestFxAccountClient20.java
+++ src/test/java/org/mozilla/gecko/background/fxa/test/TestFxAccountClient20.java
@@ -4,7 +4,6 @@
 package org.mozilla.gecko.background.fxa.test;
 
 import java.io.UnsupportedEncodingException;
-import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
@@ -13,6 +12,7 @@ import junit.framework.Assert;
 
 import org.junit.Test;
 import org.mozilla.gecko.background.fxa.FxAccountClient20;
+import org.mozilla.gecko.sync.net.BaseResource;
 
 public class TestFxAccountClient20 {
   protected static class MockFxAccountClient20 extends FxAccountClient20 {
@@ -22,8 +22,8 @@ public class TestFxAccountClient20 {
 
     // Public for testing.
     @Override
-    public URI getCreateAccountURI(final boolean getKeys, final String service) throws UnsupportedEncodingException, URISyntaxException {
-      return super.getCreateAccountURI(getKeys, service);
+    public BaseResource getBaseResource(final String path, final String... queryParameters) throws UnsupportedEncodingException, URISyntaxException {
+      return super.getBaseResource(path, queryParameters);
     }
   }
 
@@ -31,8 +31,9 @@ public class TestFxAccountClient20 {
   public void testGetCreateAccountURI() throws Exception {
     final String TEST_SERVER = "https://test.com:4430/inner/v1/";
     final MockFxAccountClient20 client = new MockFxAccountClient20(TEST_SERVER, Executors.newSingleThreadExecutor());
-    Assert.assertEquals(TEST_SERVER + "account/create?service=sync&keys=true", client.getCreateAccountURI(true, "sync").toASCIIString());
-    Assert.assertEquals(TEST_SERVER + "account/create?service=two+words", client.getCreateAccountURI(false, "two words").toASCIIString());
-    Assert.assertEquals(TEST_SERVER + "account/create?service=symbols%2F%3A%3F%2B", client.getCreateAccountURI(false, "symbols/:?+").toASCIIString());
+    Assert.assertEquals(TEST_SERVER + "account/create", client.getBaseResource("account/create").getURIString());
+    Assert.assertEquals(TEST_SERVER + "account/create?service=sync&keys=true", client.getBaseResource("account/create", "service", "sync", "keys", "true").getURIString());
+    Assert.assertEquals(TEST_SERVER + "account/create?service=two+words", client.getBaseResource("account/create", "service", "two words").getURIString());
+    Assert.assertEquals(TEST_SERVER + "account/create?service=symbols%2F%3A%3F%2B", client.getBaseResource("account/create", "service", "symbols/:?+").getURIString());
   }
 }
