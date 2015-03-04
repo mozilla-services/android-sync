1f6ab474164b2a1e0f05e2125917b7e9d7293547
Richard Newman <rnewman@twinql.com>
2015-03-04 08:46:06 -0800

    Reindent and tidy SyncAdapter.

--- src/main/java/org/mozilla/gecko/reading/ReadingListSyncAdapter.java
+++ src/main/java/org/mozilla/gecko/reading/ReadingListSyncAdapter.java
@@ -34,7 +34,6 @@ import org.mozilla.gecko.fxa.login.State.StateLabel;
 import org.mozilla.gecko.fxa.login.StateFactory;
 import org.mozilla.gecko.fxa.sync.FxAccountSyncDelegate;
 import org.mozilla.gecko.sync.net.AuthHeaderProvider;
-import org.mozilla.gecko.sync.net.BasicAuthHeaderProvider;
 import org.mozilla.gecko.sync.net.BearerAuthHeaderProvider;
 
 import android.accounts.Account;
@@ -47,8 +46,17 @@ import android.content.SyncResult;
 import android.os.Bundle;
 
 public class ReadingListSyncAdapter extends AbstractThreadedSyncAdapter {
-    static final class SyncAdapterSynchronizerDelegate implements
-      ReadingListSynchronizerDelegate {
+  private static final String LOG_TAG = ReadingListSyncAdapter.class.getSimpleName();
+  private static final long TIMEOUT_SECONDS = 60;
+  protected final ExecutorService executor;
+
+  public ReadingListSyncAdapter(Context context, boolean autoInitialize) {
+    super(context, autoInitialize);
+    this.executor = Executors.newSingleThreadExecutor();
+  }
+
+
+  static final class SyncAdapterSynchronizerDelegate implements ReadingListSynchronizerDelegate {
     private final FxAccountSyncDelegate syncDelegate;
     private final ContentProviderClient cpc;
 
@@ -89,175 +97,163 @@ public class ReadingListSyncAdapter extends AbstractThreadedSyncAdapter {
     }
   }
 
+  @Override
+  public void onPerformSync(final Account account, final Bundle extras, final String authority, final ContentProviderClient provider, final SyncResult syncResult) {
+    Logger.setThreadLogTag(ReadingListConstants.GLOBAL_LOG_TAG);
+    Logger.resetLogging();
 
-    private static final String LOG_TAG = ReadingListSyncAdapter.class.getSimpleName();
+    final Context context = getContext();
+    final AndroidFxAccount fxAccount = new AndroidFxAccount(context, account);
 
-    private static final long TIMEOUT_SECONDS = 60;
+    // If this sync was triggered by user action, this will be true.
+    final boolean isImmediate = (extras != null) &&
+        (extras.getBoolean(ContentResolver.SYNC_EXTRAS_UPLOAD, false) ||
+            extras.getBoolean(ContentResolver.SYNC_EXTRAS_MANUAL, false));
 
-    protected final ExecutorService executor;
-
-    public ReadingListSyncAdapter(Context context, boolean autoInitialize) {
-      super(context, autoInitialize);
-      this.executor = Executors.newSingleThreadExecutor();
-    }
-
-    @Override
-    public void onPerformSync(final Account account, final Bundle extras, final String authority, final ContentProviderClient provider, final SyncResult syncResult) {
-      Logger.setThreadLogTag(ReadingListConstants.GLOBAL_LOG_TAG);
-      Logger.resetLogging();
-
-      final Context context = getContext();
-      final AndroidFxAccount fxAccount = new AndroidFxAccount(context, account);
-
-      // If this sync was triggered by user action, this will be true.
-      final boolean isImmediate = (extras != null) &&
-                                  (extras.getBoolean(ContentResolver.SYNC_EXTRAS_UPLOAD, false) ||
-                                   extras.getBoolean(ContentResolver.SYNC_EXTRAS_MANUAL, false));
-
-      final CountDownLatch latch = new CountDownLatch(1);
-      final FxAccountSyncDelegate syncDelegate = new FxAccountSyncDelegate(latch, syncResult, fxAccount);
+    final CountDownLatch latch = new CountDownLatch(1);
+    final FxAccountSyncDelegate syncDelegate = new FxAccountSyncDelegate(latch, syncResult, fxAccount);
+    try {
+      final State state;
       try {
-        final State state;
-        try {
-          state = fxAccount.getState();
-        } catch (Exception e) {
-          Logger.error(LOG_TAG, "Unable to sync.", e);
-          return;
+        state = fxAccount.getState();
+      } catch (Exception e) {
+        Logger.error(LOG_TAG, "Unable to sync.", e);
+        return;
+      }
+
+      final String audience = fxAccount.getAudience();
+      final String authServerEndpoint = fxAccount.getAccountServerURI();
+
+      final SharedPreferences sharedPrefs = fxAccount.getReadingListPrefs();
+      final FxAccountClient client = new FxAccountClient20(authServerEndpoint, executor);
+      final FxAccountLoginStateMachine stateMachine = new FxAccountLoginStateMachine();
+
+      stateMachine.advance(state, StateLabel.Married, new LoginStateMachineDelegate() {
+        @Override
+        public FxAccountClient getClient() {
+          return client;
         }
 
-        final String audience = fxAccount.getAudience();
-        final String authServerEndpoint = fxAccount.getAccountServerURI();
+        @Override
+        public long getCertificateDurationInMilliseconds() {
+          return 12 * 60 * 60 * 1000;
+        }
 
-        final SharedPreferences sharedPrefs = fxAccount.getReadingListPrefs();
-        final FxAccountClient client = new FxAccountClient20(authServerEndpoint, executor);
-        final FxAccountLoginStateMachine stateMachine = new FxAccountLoginStateMachine();
+        @Override
+        public long getAssertionDurationInMilliseconds() {
+          return 15 * 60 * 1000;
+        }
 
-        stateMachine.advance(state, StateLabel.Married, new LoginStateMachineDelegate() {
-          @Override
-          public FxAccountClient getClient() {
-            return client;
-          }
+        @Override
+        public BrowserIDKeyPair generateKeyPair() throws NoSuchAlgorithmException {
+          return StateFactory.generateKeyPair();
+        }
 
-          @Override
-          public long getCertificateDurationInMilliseconds() {
-            return 12 * 60 * 60 * 1000;
-          }
+        @Override
+        public void handleTransition(Transition transition, State state) {
+          Logger.info(LOG_TAG, "handleTransition: " + transition + " to " + state.getStateLabel());
+        }
 
-          @Override
-          public long getAssertionDurationInMilliseconds() {
-            return 15 * 60 * 1000;
-          }
+        @Override
+        public void handleFinal(State state) {
+          Logger.info(LOG_TAG, "handleFinal: in " + state.getStateLabel());
+          fxAccount.setState(state);
 
-          @Override
-          public BrowserIDKeyPair generateKeyPair() throws NoSuchAlgorithmException {
-            return StateFactory.generateKeyPair();
-          }
-
-          @Override
-          public void handleTransition(Transition transition, State state) {
-            Logger.info(LOG_TAG, "handleTransition: " + transition + " to " + state.getStateLabel());
-          }
-
-          @Override
-          public void handleFinal(State state) {
-            Logger.info(LOG_TAG, "handleFinal: in " + state.getStateLabel());
-            fxAccount.setState(state);
-
-            // TODO: scheduling, notifications.
-            try {
-              if (state.getStateLabel() != StateLabel.Married) {
-                syncDelegate.handleCannotSync(state);
-                return;
-              }
-
-              final Married married = (Married) state;
-              final String assertion = married.generateAssertion(audience, JSONWebTokenUtils.DEFAULT_ASSERTION_ISSUER);
-              final String clientID = "0fddc2b28f47c2d8";   // This is magic. NIIIICKKKKKK!
-              final String scope = "profile";               // Niiiiiickkk!
-              syncWithAssertion(clientID, scope, assertion, sharedPrefs, extras);
-            } catch (Exception e) {
-              syncDelegate.handleError(e);
-              return;
-            }
-          }
-
-          private void syncWithAssertion(final String client_id, final String scope, final String assertion,
-                                         final SharedPreferences sharedPrefs, final Bundle extras) {
-            final String oauthServerUri = "https://oauth-stable.dev.lcip.org/v1";
-            final FxAccountOAuthClient10 oauthClient = new FxAccountOAuthClient10(oauthServerUri, executor);
-            oauthClient.authorization(client_id, assertion, null, scope, new RequestDelegate<FxAccountOAuthClient10.AuthorizationResponse>() {
-              @Override
-              public void handleSuccess(AuthorizationResponse result) {
-                syncWithAuthorization(result, sharedPrefs, extras);
-              }
-
-              @Override
-              public void handleFailure(FxAccountAbstractClientRemoteException e) {
-                syncDelegate.handleError(e);
-              }
-
-              @Override
-              public void handleError(Exception e) {
-                syncDelegate.handleError(e);
-              }
-            });
-          }
-
-          private void syncWithAuthorization(AuthorizationResponse authResponse,
-                                             SharedPreferences sharedPrefs,
-                                             Bundle extras) {
-            final AuthHeaderProvider auth = new BearerAuthHeaderProvider(authResponse.access_token);
-
-            final String endpointString = ReadingListConstants.DEFAULT_DEV_ENDPOINT;
-            final URI endpoint;
-            try {
-              endpoint = new URI(endpointString);
-            } catch (URISyntaxException e) {
-              // Should never happen.
-              Logger.error(LOG_TAG, "Unexpected malformed URI for reading list service: " + endpointString);
-              syncDelegate.handleError(e);
+          // TODO: scheduling, notifications.
+          try {
+            if (state.getStateLabel() != StateLabel.Married) {
+              syncDelegate.handleCannotSync(state);
               return;
             }
 
-            final PrefsBranch branch = new PrefsBranch(sharedPrefs, "readinglist.");
-            final ReadingListClient remote = new ReadingListClient(endpoint, auth);
-            final ContentProviderClient cpc = getContentProviderClient(context);     // TODO: make sure I'm always released!
-
-            final ReadingListStorage local = new LocalReadingListStorage(cpc);
-            final ReadingListSynchronizer synchronizer = new ReadingListSynchronizer(branch, remote, local);
-
-            synchronizer.syncAll(new SyncAdapterSynchronizerDelegate(syncDelegate, cpc));
-            // TODO: backoffs, and everything else handled by a SessionCallback.
+            final Married married = (Married) state;
+            final String assertion = married.generateAssertion(audience, JSONWebTokenUtils.DEFAULT_ASSERTION_ISSUER);
+            final String clientID = "0fddc2b28f47c2d8";   // This is magic. NIIIICKKKKKK!
+            final String scope = "profile";               // Niiiiiickkk!
+            syncWithAssertion(clientID, scope, assertion, sharedPrefs, extras);
+          } catch (Exception e) {
+            syncDelegate.handleError(e);
+            return;
           }
-        });
+        }
 
-        latch.await(TIMEOUT_SECONDS, TimeUnit.SECONDS);
-      } catch (Exception e) {
-        Logger.error(LOG_TAG, "Got error syncing.", e);
-        syncDelegate.handleError(e);
-      }
-      /*
-       * TODO:
-       * * Account error notifications. How do we avoid these overlapping with Sync?
-       * * Pickling. How do we avoid pickling twice if you use both Sync and RL?
-       */
+        private void syncWithAssertion(final String client_id, final String scope, final String assertion,
+                                       final SharedPreferences sharedPrefs, final Bundle extras) {
+          final String oauthServerUri = "https://oauth-stable.dev.lcip.org/v1";
+          final FxAccountOAuthClient10 oauthClient = new FxAccountOAuthClient10(oauthServerUri, executor);
+          oauthClient.authorization(client_id, assertion, null, scope, new RequestDelegate<FxAccountOAuthClient10.AuthorizationResponse>() {
+            @Override
+            public void handleSuccess(AuthorizationResponse result) {
+              syncWithAuthorization(result, sharedPrefs, extras);
+            }
 
-      /*
-       * TODO:
-       * * Auth.
-       * * Server URI lookup.
-       * * Syncing.
-       * * Error handling.
-       * * Backoff and retry-after.
-       * * Sync scheduling.
-       * * Forcing syncs/interactive use.
-       */
+            @Override
+            public void handleFailure(FxAccountAbstractClientRemoteException e) {
+              syncDelegate.handleError(e);
+            }
+
+            @Override
+            public void handleError(Exception e) {
+              syncDelegate.handleError(e);
+            }
+          });
+        }
+
+        private void syncWithAuthorization(AuthorizationResponse authResponse,
+                                           SharedPreferences sharedPrefs,
+                                           Bundle extras) {
+          final AuthHeaderProvider auth = new BearerAuthHeaderProvider(authResponse.access_token);
+
+          final String endpointString = ReadingListConstants.DEFAULT_DEV_ENDPOINT;
+          final URI endpoint;
+          try {
+            endpoint = new URI(endpointString);
+          } catch (URISyntaxException e) {
+            // Should never happen.
+            Logger.error(LOG_TAG, "Unexpected malformed URI for reading list service: " + endpointString);
+            syncDelegate.handleError(e);
+            return;
+          }
+
+          final PrefsBranch branch = new PrefsBranch(sharedPrefs, "readinglist.");
+          final ReadingListClient remote = new ReadingListClient(endpoint, auth);
+          final ContentProviderClient cpc = getContentProviderClient(context);     // TODO: make sure I'm always released!
+
+          final ReadingListStorage local = new LocalReadingListStorage(cpc);
+          final ReadingListSynchronizer synchronizer = new ReadingListSynchronizer(branch, remote, local);
+
+          synchronizer.syncAll(new SyncAdapterSynchronizerDelegate(syncDelegate, cpc));
+          // TODO: backoffs, and everything else handled by a SessionCallback.
+        }
+      });
+
+      latch.await(TIMEOUT_SECONDS, TimeUnit.SECONDS);
+    } catch (Exception e) {
+      Logger.error(LOG_TAG, "Got error syncing.", e);
+      syncDelegate.handleError(e);
     }
+    /*
+     * TODO:
+     * * Account error notifications. How do we avoid these overlapping with Sync?
+     * * Pickling. How do we avoid pickling twice if you use both Sync and RL?
+     */
+
+    /*
+     * TODO:
+     * * Auth.
+     * * Server URI lookup.
+     * * Syncing.
+     * * Error handling.
+     * * Backoff and retry-after.
+     * * Sync scheduling.
+     * * Forcing syncs/interactive use.
+     */
+  }
 
 
-    private ContentProviderClient getContentProviderClient(Context context) {
-        final ContentResolver contentResolver = context.getContentResolver();
-        final ContentProviderClient client = contentResolver.acquireContentProviderClient(ReadingListItems.CONTENT_URI);
-        return client;
-    }
+  private ContentProviderClient getContentProviderClient(Context context) {
+    final ContentResolver contentResolver = context.getContentResolver();
+    final ContentProviderClient client = contentResolver.acquireContentProviderClient(ReadingListItems.CONTENT_URI);
+    return client;
+  }
 }
