fdeff3b54707256bd4aaf1dc0a75d6198adacac3
Richard Newman <rnewman@twinql.com>
2015-03-12 14:50:50 -0700

    Synchronizer delegate commenting.

--- src/main/java/org/mozilla/gecko/reading/ReadingListSynchronizerDelegate.java
+++ src/main/java/org/mozilla/gecko/reading/ReadingListSynchronizerDelegate.java
@@ -7,12 +7,16 @@ package org.mozilla.gecko.reading;
 import java.util.Collection;
 
 public interface ReadingListSynchronizerDelegate {
+  // Called on failure.
   void onUnableToSync(Exception e);
 
+  // These are called sequentially, or not at all
+  // if a failure occurs.
   void onStatusUploadComplete(Collection<String> uploaded, Collection<String> failed);
   void onNewItemUploadComplete(Collection<String> uploaded, Collection<String> failed);
   void onDownloadComplete();
   void onModifiedUploadComplete();
 
+  // If no failure occurred, called at the end.
   void onComplete();
 }
