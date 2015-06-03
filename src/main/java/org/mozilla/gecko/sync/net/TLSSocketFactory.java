e3138ac26baa0a77319b691162dcfce2cabfffa3
Richard Newman <rnewman@twinql.com>
2015-06-02 20:36:17 -0700

    Logging in TLSSocketFactory.

--- src/main/java/org/mozilla/gecko/sync/net/TLSSocketFactory.java
+++ src/main/java/org/mozilla/gecko/sync/net/TLSSocketFactory.java
@@ -5,7 +5,10 @@
 package org.mozilla.gecko.sync.net;
 
 import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSocket;
@@ -13,8 +16,11 @@ import javax.net.ssl.SSLSocket;
 import org.mozilla.gecko.background.common.GlobalConstants;
 import org.mozilla.gecko.background.common.log.Logger;
 
+import ch.boye.httpclientandroidlib.HttpHost;
+import ch.boye.httpclientandroidlib.conn.ConnectTimeoutException;
 import ch.boye.httpclientandroidlib.conn.ssl.SSLSocketFactory;
 import ch.boye.httpclientandroidlib.params.HttpParams;
+import ch.boye.httpclientandroidlib.protocol.HttpContext;
 
 public class TLSSocketFactory extends SSLSocketFactory {
   private static final String LOG_TAG = "TLSSocketFactory";
@@ -23,7 +29,7 @@ public class TLSSocketFactory extends SSLSocketFactory {
   private static String[] cipherSuites = GlobalConstants.DEFAULT_CIPHER_SUITES;
 
   public TLSSocketFactory(SSLContext sslContext) {
-    super(sslContext);
+    super(sslContext, GlobalConstants.DEFAULT_PROTOCOLS, GlobalConstants.DEFAULT_CIPHER_SUITES, null);
   }
 
   /**
@@ -52,11 +58,69 @@ public class TLSSocketFactory extends SSLSocketFactory {
     }
   }
 
+  private static void logSocket(Socket socket) {
+    if (socket instanceof SSLSocket) {
+      Logger.info(LOG_TAG, "XXX: Protocols: ");
+      for (String protocol: ((SSLSocket) socket).getEnabledProtocols()) {
+        Logger.info(LOG_TAG, "XXX:   --- " + protocol);
+      }
+      Logger.info(LOG_TAG, "XXX: Cipher suites: ");
+      for (String suite: ((SSLSocket) socket).getEnabledCipherSuites()) {
+        Logger.info(LOG_TAG, "XXX:   --- " + suite);
+      }
+    }
+  }
+  private static Socket adjustSocket(SSLSocket socket) {
+    socket.setEnabledProtocols(GlobalConstants.DEFAULT_PROTOCOLS);
+    //setEnabledCipherSuites(socket);
+    logSocket(socket);
+    return socket;
+  }
+
+  @Override
+  public Socket createSocket() throws IOException {
+    Logger.info(LOG_TAG, "XXX: Creating custom socket without params.");
+    SSLSocket socket = (SSLSocket) super.createSocket();
+    return adjustSocket(socket);
+  }
+
   @Override
   public Socket createSocket(HttpParams params) throws IOException {
+    Logger.info(LOG_TAG, "XXX: Creating custom socket.");
     SSLSocket socket = (SSLSocket) super.createSocket(params);
-    socket.setEnabledProtocols(GlobalConstants.DEFAULT_PROTOCOLS);
-    setEnabledCipherSuites(socket);
-    return socket;
+    return adjustSocket(socket);
+  }
+
+  @Override
+  public Socket connectSocket(Socket socket, InetSocketAddress remoteAddress,
+                              InetSocketAddress localAddress, HttpParams params)
+                                                                                throws IOException,
+                                                                                UnknownHostException,
+                                                                                ConnectTimeoutException {
+    Logger.info(LOG_TAG, "XXX connectSocket A");
+    logSocket(socket);
+    return super.connectSocket(socket, remoteAddress, localAddress, params);
+  }
+
+  @Override
+  public Socket connectSocket(Socket socket, String host, int port,
+                              InetAddress local, int localPort,
+                              HttpParams params) throws IOException,
+                                                UnknownHostException,
+                                                ConnectTimeoutException {
+    Logger.info(LOG_TAG, "XXX connectSocket B");
+    logSocket(socket);
+    return super.connectSocket(socket, host, port, local, localPort, params);
+  }
+
+  @Override
+  public Socket connectSocket(int connectTimeout, Socket socket, HttpHost host,
+                              InetSocketAddress remoteAddress,
+                              InetSocketAddress localAddress,
+                              HttpContext context) throws IOException {
+    Logger.info(LOG_TAG, "XXX connectSocket C");
+    logSocket(socket);
+    return super.connectSocket(connectTimeout, socket, host, remoteAddress,
+                               localAddress, context);
   }
 }
